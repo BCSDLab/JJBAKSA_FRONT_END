e26144b81aee3fc43d50f5a35144b2d376d592f6
minjaeKim <smallkdb@gmail.com>
2022-09-08 01:08:15 +0900

    chore: envision folder and file frame

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Home from 'pages/Home';
+import TextEditor from 'pages/textEditor/textEditor';
 import { Routes, Route } from 'react-router-dom';
 
 function App(): JSX.Element {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/te" element={<TextEditor />} />
     </Routes>
   );
 }
---
+++ src/pages/textEditor/textEditor.tsx
@@ -0,0 +1,11 @@
+import styles from './TextEditor.module.scss';
+
+function TextEditor(): JSX.Element {
+  return (
+    <div className={styles.tamplate}>
+      example
+    </div>
+  );
+}
+
+export default TextEditor;
