f8192a12588dfd6dad9e33527440512d52de022d
minjaeKim <smallkdb@gmail.com>
2022-09-08 03:45:39 +0900

    chore: 파일 이름 및 import 경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Home from 'pages/Home';
-import TextEditor from 'pages/textEditor/textEditor';
+import TextEditor from 'pages/TextEditor';
 import { Routes, Route } from 'react-router-dom';
 
 function App(): JSX.Element {
