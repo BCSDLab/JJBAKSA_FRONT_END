4a2c5ca0148f3be494e32192ddaa2f66d5561eb1
ChoiWonBeen <zad1264@naver.com>
2022-09-08 21:16:19 +0900

    feat: 모바일 하단 네비게이션 추가

---
+++ src/components/common/BottomNavigation/index.tsx
@@ -0,0 +1,52 @@
+import { ReactComponent as HomeIcon } from 'assets/svg/home.svg';
+import { ReactComponent as WriteIcon } from 'assets/svg/write.svg';
+import { ReactComponent as MyPageIcon } from 'assets/svg/my-page.svg';
+import { Link, useLocation } from 'react-router-dom';
+import cn from 'utils/ts/classNames';
+import styles from './BottomNavigation.module.scss';
+
+const navTabs = [
+  {
+    pathname: '/',
+    icon: HomeIcon,
+    text: '홈',
+  },
+  {
+    pathname: '/post',
+    icon: WriteIcon,
+    text: '글쓰기',
+  },
+  {
+    pathname: '/profile',
+    icon: MyPageIcon,
+    text: '마이페이지',
+  },
+];
+
+function BottomNavigation(): JSX.Element {
+  const { pathname } = useLocation();
+
+  return (
+    <nav className={styles['bottom-navigation']}>
+      <ul className={styles.tab}>
+        {navTabs.map((tab) => (
+          <li
+            aria-label={`${tab.text}으로 이동`}
+            key={tab.text}
+            className={cn({
+              [styles.tab__item]: true,
+              [styles['tab__item--focused']]: pathname === tab.pathname,
+            })}
+          >
+            <Link className={styles.tab__link} to={tab.pathname}>
+              <tab.icon className={styles.tab__icon} />
+              <div className={styles.tab__text}>{tab.text}</div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default BottomNavigation;
