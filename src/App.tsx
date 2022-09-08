8b4ccf2b7aa26dcd98679289a0afdb156a1210bb
jaewoogwak <jaewoo010207@gmail.com>
2022-09-08 15:59:09 +0900

    chore : 기본 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Home from 'pages/Home';
+import Search from 'pages/Search';
 import { Routes, Route } from 'react-router-dom';
 
 function App(): JSX.Element {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/search" element={<Search />} />
     </Routes>
   );
 }
---
+++ src/pages/Search/index.tsx
@@ -0,0 +1,11 @@
+import styles from './Search.module.scss';
+
+function Search(): JSX.Element {
+  return (
+    <div className={styles.search}>
+      SearchBar
+    </div>
+  );
+}
+
+export default Search;
