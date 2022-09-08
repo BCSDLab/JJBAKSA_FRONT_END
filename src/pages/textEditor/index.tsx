1908507031d33aba446eb89d6e3130a83d865592
minjaeKim <smallkdb@gmail.com>
2022-09-08 21:38:49 +0900

    refactor: className 을 bem 형식에 맞게 변경

--- src/pages/textEditor/index.tsx
+++ src/pages/textEditor/index.tsx
@@ -3,16 +3,16 @@ import styles from './TextEditor.module.scss';
 function TextEditor(): JSX.Element {
   return (
     <div className={styles.template}>
-      <div className={styles.editor__title}>
-        <button type="button" className={styles.before__button}>{'<'}</button>
-        <div className={styles.editor__shopname}>여기에 가게명</div>
-        <div className={styles.starcontainer}>starContainer</div>
+      <div className={styles.header}>
+        <button type="button" className={styles.header__button}>{'<'}</button>
+        <div className={styles.header__shopname}>여기에 가게명</div>
+        <div className={styles['header__star-container']}>starContainer</div>
       </div>
-      <div className={styles.editor__main}>에디터가 들어갈 공간</div>
-      <div className={styles.editor__tools}>
+      <div className={styles.editor}>에디터가 들어갈 공간</div>
+      <div className={styles.item}>
         <div>
-          <button type="button" className={styles.picture__button}>사진</button>
-          <button type="button" className={styles.texttool__button}>T</button>
+          <button type="button" className={styles.item__button}>사진</button>
+          <button type="button" className={styles.item__button}>T</button>
         </div>
         <button type="button" className={styles.save__button}>저장</button>
       </div>
