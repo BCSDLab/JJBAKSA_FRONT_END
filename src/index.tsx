52584e571ac7c0ab3ca9ef439e6d0ef0ea5e8433
minjaeKim <smallkdb@gmail.com>
2022-09-08 22:36:58 +0900

    style: global 영역에 SUIT 폰트 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './assets/fonts/SUIT.scss';
 import './index.scss';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
