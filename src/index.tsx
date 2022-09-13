d291b65ba3b8b393e9de012284d76ba71b0c4f7b
minjaeKim <smallkdb@gmail.com>
2022-09-13 12:10:42 +0900

    fix: font-face minxin 추가, 폴더 및 이름 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/fonts/SUIT.scss';
+import './styles/fonts.scss';
 import './index.scss';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
