551a69cbba13e6dd82cfdc04824ff8a1bc0e90e7
minjaeKim <smallkdb@gmail.com>
2022-09-13 17:32:57 +0900

    refactor: index.tsx의 import scss 파일 간소화

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/fonts.scss';
 import './index.scss';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
