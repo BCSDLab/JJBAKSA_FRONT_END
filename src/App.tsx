7cc4119651192ab0e2ba8065ce59623880961299
hana <rlagksk8428@gmail.com>
2022-09-19 23:14:51 +0900

    feat: setting Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import DefaultLayout from 'layout/DefaultLayout';
+import SignUpForm from 'pages/Auth/SignUpPage/SignUpForm';
+import TermsOfService from 'pages/Auth/SignUpPage/TermsOfService';
 import Home from 'pages/Home';
 import { Routes, Route } from 'react-router-dom';
 
@@ -6,6 +8,8 @@ function App(): JSX.Element {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout><Home /></DefaultLayout>} />
+      <Route path="/TermsOfService" element={<TermsOfService />} />
+      <Route path="/SignUp" element={<SignUpForm />} />
     </Routes>
   );
 }
