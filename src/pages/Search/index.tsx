1ab85404171ff2348fda32bf8fc787332ad820f9
jaewoogwak <jaewoo010207@gmail.com>
2022-09-21 10:27:07 +0900

    fix: 웹 접근성 적용 및 시멘틱 태그 사용

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,6 +1,6 @@
 import { ReactComponent as PreviousIcon } from 'assets/svg/previous.svg';
 import { ReactComponent as LensIcon } from 'assets/svg/lens.svg';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import robots from './data';
 import styles from './Search.module.scss';
@@ -18,12 +18,13 @@ interface IItem {
 }
 const RECOMMEND_TEXT = [
   '오늘은\n어떤 음식이 땡기나요?',
-  '기름진 음식이 그리운 날!',
+  '기름진 음식이\n그리운 날!',
 ];
 
 function Search(): JSX.Element {
   const [text, setText] = useState('');
   const [isFocus, setIsFocus] = useState(false);
+  const recommendIdx = useRef(new Date().getSeconds() % 2);
   const list = robots;
   const navigate = useNavigate();
 
@@ -36,33 +37,39 @@ function Search(): JSX.Element {
   };
   return (
     <div className={styles.search}>
-      <div className={styles['search-header']}>
+      <div aria-label="상단 바" className={styles['search-header']}>
         <button
           className={styles['search-header__previous-button']}
           type="button"
-          aria-label="이전 버튼"
+          aria-label="메인 페이지로 돌아가는 버튼"
           onClick={() => navigate('/')}
         >
           <PreviousIcon className={styles['search-header__previous-icon']} />
         </button>
-        <div className={styles['search-header__text']}>검색</div>
+        <h1 className={styles['search-header__text']}>검색</h1>
       </div>
-      {!isFocus && <h1 className={styles['search-recommend']}>{RECOMMEND_TEXT[0]}</h1>}
-      <div className={styles['search-bar']}>
-        <input onFocus={focusHandler} onBlur={blurHandler} className={styles['search-bar__input']} placeholder="검색어를 입력해주세요" value={text} onChange={(event) => setText(event.target.value)} />
-        <LensIcon className={styles['search-bar__icon']} />
+      <div role="main">
+        <section>
+          {!isFocus && <h1 className={styles['search-recommend']}>{RECOMMEND_TEXT[recommendIdx.current]}</h1>}
+          <label aria-label="검색창" htmlFor="searchBarInput" className={styles['search-bar']}>
+            <input id="searchBarInput" onFocus={focusHandler} onBlur={blurHandler} className={styles['search-bar__input']} placeholder="검색어를 입력해주세요" value={text} onChange={(event) => setText(event.target.value)} />
+            <LensIcon className={styles['search-bar__icon']} />
+          </label>
+          <div aria-label="맛집 트렌드 해시태그" className={styles['search-trend']}>
+            <strong>TRENDING</strong>
+            <div className={styles['search-trend__tag-list--wrapper']}>
+              <ul className={styles['search-trend__tag-list']}>
+                <b className={styles['search-trend__tag-list__tag']}>{hash_tag.map((tag) => tag.text).join(' ')}</b>
+                <b className={styles['search-trend__tag-list__tag']}>{hash_tag.map((tag) => tag.text).join(' ')}</b>
+                <b className={styles['search-trend__tag-list__tag']}>{hash_tag.map((tag) => tag.text).join(' ')}</b>
+              </ul>
+            </div>
+          </div>
+          <menu aria-label="검색 결과" className={styles['search-list']}>
+            {text === '' ? null : list?.filter((item) => item.title.includes(text)).map((item : IItem) => <li key={item.title} className={styles['search-list__item']}>{item.title}</li>)}
+          </menu>
+        </section>
       </div>
-      <div className={styles['search-trend']}>
-        <strong>TRENDING</strong>
-        <div className={styles['search-trend__tag-list--wrapper']}>
-          <ul className={styles['search-trend__tag-list']}>
-            <div className={styles['search-trend__tag-list__tag']}>{hash_tag.map((tag) => tag.text).join(' ')}</div>
-          </ul>
-        </div>
-      </div>
-      <ul className={styles['search-list']}>
-        {text === '' ? null : list?.filter((item) => item.title.includes(text)).map((item : IItem) => <li key={item.title} className={styles['search-list__item']}>{item.title}</li>)}
-      </ul>
     </div>
   );
 }
