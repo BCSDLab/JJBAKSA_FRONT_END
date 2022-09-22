476a062713708ec62d841f02881a6fff433a1a9e
kimeodml <kimeodml@gmail.com>
2022-09-22 21:57:54 +0900

    refactor: 요소 변경 및 불필요한 추론 제거

--- src/components/Auth/AuthTitle/index.tsx
+++ src/components/Auth/AuthTitle/index.tsx
@@ -2,11 +2,11 @@ import { ReactComponent as LogoIcon } from 'assets/svg/logo.svg';
 import { Link } from 'react-router-dom';
 import styles from './AuthTitle.module.scss';
 
-function AuthTitle(): JSX.Element {
+function AuthTitle() {
   return (
     <div className={styles.title}>
       <Link className={styles.title__logo} to="/"><LogoIcon /></Link>
-      <div className={styles.title__text}>쩝쩝박사</div>
+      <h1 className={styles.title__text}>쩝쩝박사</h1>
     </div>
   );
 }
