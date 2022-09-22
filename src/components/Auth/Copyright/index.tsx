7342a79d210e78787ee0a9059268ed15c73d0b2c
kimeodml <kimeodml@gmail.com>
2022-09-22 23:45:54 +0900

    refactor: Copyright 특수문자 제거 및 scss 수정

--- src/components/Auth/Copyright/index.tsx
+++ src/components/Auth/Copyright/index.tsx
@@ -3,11 +3,11 @@ import styles from './Copyright.module.scss';
 function Copyright(): JSX.Element {
   return (
     <div className={styles.copyright}>
-      COPYRIGHT ⓒ&nbsp;
-      {
-        new Date().getFullYear()
-      }
-      &nbsp;BY BCSDLab ALL RIGHTS RESERVED.
+      COPYRIGHT ⓒ
+      {' '}
+      { new Date().getFullYear() }
+      {' '}
+      BY BCSDLab ALL RIGHTS RESERVED.
     </div>
   );
 }
