c827c2ac97144ad626d75ebee95381b669d1c8ae
kimeodml <kimeodml@gmail.com>
2022-09-23 20:19:48 +0900

    refactor: Home 복구

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import DefaultLayout from 'layout/DefaultLayout';
 import LoginPage from 'pages/Auth/LoginPage';
 import Home from 'pages/Home';
 import { Routes, Route } from 'react-router-dom';
@@ -5,7 +6,7 @@ import { Routes, Route } from 'react-router-dom';
 function App(): JSX.Element {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<DefaultLayout><Home /></DefaultLayout>} />
       <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
