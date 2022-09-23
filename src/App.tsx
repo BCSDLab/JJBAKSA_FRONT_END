9a110cc01fc42146d18faf557d0182a01e2aded6
jaewoogwak <jaewoo010207@gmail.com>
2022-09-23 15:18:48 +0900

    fix: PR 코드리뷰 적용 #1

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,13 @@ import DefaultLayout from 'layout/DefaultLayout';
 import Home from 'pages/Home';
 import Search from 'pages/Search';
 import { Routes, Route } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
 
-const queryClient = new QueryClient();
 function App(): JSX.Element {
   return (
-
-    <QueryClientProvider client={queryClient}>
-      <Routes>
-        <Route path="/" element={<DefaultLayout><Home /></DefaultLayout>} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
-    </QueryClientProvider>
+    <Routes>
+      <Route path="/" element={<DefaultLayout><Home /></DefaultLayout>} />
+      <Route path="/search" element={<Search />} />
+    </Routes>
   );
 }
 
