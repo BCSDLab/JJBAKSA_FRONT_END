515fc1206bf1644bff9e85689a8804460e984603
hana <rlagksk8428@gmail.com>
2022-09-26 21:52:31 +0900

    fix: change location state type

--- src/pages/Auth/SignUpPage/hooks/useRouteCheck.ts
+++ src/pages/Auth/SignUpPage/hooks/useRouteCheck.ts
@@ -1,17 +1,20 @@
 import { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
+type RouteCheckValue = {
+  [key: string]: boolean
+};
+
 const useRouteCheck = (checkValue: string, route: string) => {
   const location = useLocation();
   const navigate = useNavigate();
 
   useEffect(() => {
-    const state = location.state as any;
+    const state = location.state as RouteCheckValue;
     if (!state || !(checkValue in state)) {
       navigate(route);
     }
   }, [location.state, navigate, route, checkValue]);
 };
-//
 
 export default useRouteCheck;
