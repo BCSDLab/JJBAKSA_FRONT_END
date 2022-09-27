710c7334ce672dd7dbc037680ddf9b33bd3e604e
minjaeKim <smallkdb@gmail.com>
2022-09-28 07:18:39 +0900

    style: useContext 제네릭 네이밍 수정

--- src/utils/hooks/useContext.ts
+++ src/utils/hooks/useContext.ts
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 
-export default function useStarContainerContext<T>(
-  ContextValue: React.Context<T | null>,
+export default function useStarContainerContext<Type>(
+  ContextValue: React.Context<Type | null>,
 ) {
   const state = useContext(ContextValue);
   if (!state) throw new Error('Provider not found');
