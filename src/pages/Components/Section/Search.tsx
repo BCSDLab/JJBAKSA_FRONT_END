00db5d2ca9915aed82ec33b7645945541d6e7053
chaeseungyun <thslr57@naver.com>
2022-09-28 11:08:11 +0900

    fix: 아이디 찾기 페이지 재사용

--- src/pages/Components/Section/Search.tsx
+++ src/pages/Components/Section/Search.tsx
@@ -1,18 +1,21 @@
 import { useForm } from 'react-hook-form';
 import { ReactComponent as Prev } from 'assets/svg/prev-icon.svg';
+import { ReactComponent as Caution } from 'assets/svg/warning.svg';
 import style from './SearchPage.module.scss';
 
 const emailPattern = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i; // 이메일 형식 유효성 검사 패턴
-
-export default function SearchID(): JSX.Element {
+interface Isearch {
+  search: string
+}
+export default function Search({ search }: Isearch): JSX.Element {
   const {
     register,
+    getValues,
     handleSubmit,
     formState: { isSubmitting, errors },
   } = useForm({
     mode: 'onChange',
   });
-  console.log(errors);
   return (
     <div className={style.layout}>
       <div className={style.container}>
@@ -20,20 +23,30 @@ export default function SearchID(): JSX.Element {
           <div className={style.back_icon}>
             <Prev />
           </div>
+          {search === 'id' && (
           <p>
             아이디 찾을 때
             <br />
             사용할 이메일을 입력해주세요.
           </p>
+          )}
+          {search === 'password' && (
+          <p>
+            비밀번호를 찾을 때
+            <br />
+            사용할 이메일을 입력해주세요.
+          </p>
+          )}
         </div>
-        <div
-          className={[style.error_message, style.animation].join(' ')}
-        >
-          계정이 존재하지 않거나
-          <br />
-          올바르지 않은 이메일 형식입니다.
+        <div className={style.error_message}>
+          {errors.email && (
+          <span className={style.caution}>
+            <Caution />
+            올바르지 않은 이메일 형식입니다.
+          </span>
+          )}
         </div>
-        <form onSubmit={handleSubmit((data) => console.log(data))}>
+        <form onSubmit={handleSubmit((data) => (data))}>
           <div className={style.input_label}>
             <div className={style.email}>이메일</div>
             <input
@@ -42,15 +55,15 @@ export default function SearchID(): JSX.Element {
               className={style.input_style_radius}
               id="email"
               {...register('email', {
-                required: 'email은 필수값입니다.',
+                required: true,
                 pattern: {
                   value: emailPattern,
-                  message: 'email 형식으로 작성해주세요.',
+                  message: '올바른 email 형식이 아닙니다.',
                 },
               })}
             />
           </div>
-          <button type="submit" disabled={isSubmitting} className={style.active}>인증번호 보내기</button>
+          <button type="submit" disabled={isSubmitting} className={getValues('email') !== undefined && errors.email?.message === undefined ? style.active : style.inactive}>인증번호 보내기</button>
         </form>
       </div>
     </div>
