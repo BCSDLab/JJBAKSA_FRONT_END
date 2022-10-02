e85e938a963626397d6afcae59fd845560582c11
minjaeKim <smallkdb@gmail.com>
2022-10-02 10:11:57 +0900

    refactor: 모듈 분리 및 네이밍 일부 수정

--- src/components/StarRating/index.tsx
+++ src/components/StarRating/index.tsx
@@ -1,81 +1,9 @@
-import { ReactComponent as Star } from 'assets/svg/star.svg';
-import {
-  createContext, useMemo, useState,
-} from 'react';
-import useContext from '../../utils/hooks/useContext';
-import styles from './StarRating.module.scss';
+import { useMemo, useState } from 'react';
+import StarRateContext from './StarRateContext';
+import EnterStarRateContainer from './EnterStarRateContainer';
+import LeaveStarRateContainer from './LeaveStarRateContainer';
 
-interface StarRateContextType {
-  handleMouseEnter: () => void;
-  handleMouseLeave: () => void;
-  handleClick: (num: number) => void;
-}
-interface Props {
-  onClick: () => void;
-}
-
-export const StarRateContext = createContext<StarRateContextType | null>(null);
-
-function LeaveStarRateContainer({ rating }: { rating: number }) {
-  const { handleMouseEnter } = useContext(StarRateContext);
-  return (
-    <div
-      className={styles.starRateContainer}
-      onMouseEnter={handleMouseEnter}
-    >
-      { [1, 2, 3, 4, 5].map((num) => (
-        <button
-          key={num}
-          type="button"
-          className={styles.wrapper}
-        >
-          <Star
-            key={num}
-            className={styles.wrapper__star}
-            fill={rating >= num ? '#ff7f23' : '#eee'}
-          />
-        </button>
-      ))}
-    </div>
-  );
-}
-
-function EnterStarRateContainer({ rating }: { rating: number }) {
-  const {
-    handleMouseLeave, handleClick,
-  } = useContext(StarRateContext);
-  const [starNum, setStarNum] = useState(rating);
-
-  return (
-    <div
-      className={styles.starRateContainer}
-      onMouseLeave={() => {
-        handleMouseLeave();
-        setStarNum(0);
-      }}
-    >
-      {[1, 2, 3, 4, 5].map((num) => (
-        <button
-          key={num}
-          type="button"
-          className={styles.wrapper}
-          onMouseEnter={() => setStarNum(num)}
-          onClick={() => {
-            handleClick(num);
-          }}
-        >
-          <Star
-            key={num}
-            className={styles.wrapper__star}
-            fill={starNum >= num ? '#ff7f23' : '#eee'}
-          />
-        </button>
-      ))}
-    </div>
-  );
-}
-
-export default function StarRating({ onClick }: Props) {
+export default function StarRating({ onClick }: { onClick: () => void }) {
   const [rating, setRating] = useState(0);
   const [isMouseEnter, setIsMouseEnter] = useState(false);
 
