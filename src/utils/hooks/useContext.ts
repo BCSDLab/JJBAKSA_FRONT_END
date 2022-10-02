f1917b8868e432a9402434f85327e5bd425fd1c9
minjaeKim <smallkdb@gmail.com>
2022-10-02 10:13:22 +0900

    refactor: 네이밍 추상화, 엄격한 분기문으로 수정

--- src/utils/hooks/useContext.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-
-export default function useStarContainerContext<Type>(
-  ContextValue: React.Context<Type | null>,
-) {
-  const state = useContext(ContextValue);
-  if (!state) throw new Error('Provider not found');
-  return state;
-}
