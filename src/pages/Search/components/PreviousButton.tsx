3518289247614a0411f090d20086f3a67efdd7a0
jaewoogwak <jaewoo010207@gmail.com>
2022-10-09 15:33:55 +0900

    fix: 라우팅 핸들러 함수 추가

--- src/pages/Search/components/PreviousButton.tsx
+++ src/pages/Search/components/PreviousButton.tsx
@@ -1,19 +1,23 @@
-import { Link } from 'react-router-dom';
 import { ReactComponent as PreviousIcon } from 'assets/svg/previous.svg';
+import { useNavigate } from 'react-router-dom';
 import styles from '../Search.module.scss';
 
 interface Navigate {
-  to? : string;
+  to? : string | Number;
 }
 
-export default function PreviousButton({ to } : Navigate) {
+export default function PreviousButton({ to = -1 } : Navigate) {
+  const navigate = useNavigate();
+
+  function routingHandler() {
+    // if (to === undefined) {
+    //   navigate(-1);
+    // } else navigate(to);
+    navigate(to as string);
+  }
   return (
-    <Link to={`${to}`} className={styles['search-nav__button--previous']}>
+    <button type="button" onClick={routingHandler} className={styles['search-nav__button--previous']}>
       <PreviousIcon title="이전 페이지로 이동" />
-    </Link>
+    </button>
   );
 }
-
-PreviousButton.defaultProps = {
-  to: '/',
-};
