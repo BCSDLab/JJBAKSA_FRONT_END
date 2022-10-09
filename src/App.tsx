54ca2647baddfa4bfd5f371d3fd29e96f07c82d4
jaewoogwak <jaewoo010207@gmail.com>
2022-10-09 15:44:29 +0900

    fix: 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import LoginPage from 'pages/Auth';
 import Home from 'pages/Home';
 import Post from 'pages/Post';
 import Search from 'pages/Search';
+import SearchQueryItemList from 'pages/Search/components/SearchQueryItemList';
 import { Routes, Route } from 'react-router-dom';
 
 function App(): JSX.Element {
@@ -14,6 +15,8 @@ function App(): JSX.Element {
         <Route path="/post" element={<Post />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/search" element={<Search />} />
+        <Route path="/search/:searchQuery" element={<SearchQueryItemList />} />
+
       </Routes>
       <Toast />
     </>
