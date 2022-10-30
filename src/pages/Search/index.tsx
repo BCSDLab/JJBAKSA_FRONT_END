261bcbbdbdef418860864b6e01f5d25942d4bd78
jaewoogwak <jaewoo010207@gmail.com>
2022-10-30 23:45:41 +0900

    refactor: 컴포넌트 분리

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,12 +1,13 @@
-import { ReactComponent as LensIcon } from 'assets/svg/lens.svg';
 import { useSearchParams } from 'react-router-dom';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import cn from 'utils/ts/classNames';
-import PreviousButton from 'components/PreviousButton/PreviousButton';
 import RollingBanner from './components/RollingBanner';
 import list from './static/data';
 import styles from './Search.module.scss';
-import recommend_text from './static/recommend';
+import Suggestion from './components/Suggestion';
+import Navigation from './components/Navigation';
+import Recommend from './components/Recommend';
+import SearchInput from './components/SearchInput';
 
 type CurrentMode = string | null;
 
@@ -39,8 +40,6 @@ function Search(): JSX.Element {
     text, mode, handleText, changeSearchMode, changeTrendingMode,
   } = useSearchForm(currentMode || 'trending');
 
-  const recommendIdx = useRef(new Date().getSeconds() % 2);
-
   return (
     <div className={styles.search}>
       <section className={cn({
@@ -48,41 +47,17 @@ function Search(): JSX.Element {
         [styles['search-wrapper--search']]: mode === 'search',
       })}
       >
-        <nav className={styles['search-nav']}>
-          <div className={styles['search-nav__button']}>
-            <PreviousButton />
-          </div>
-          <h1 className={styles['search-nav__text']}>검색</h1>
-        </nav>
-        {mode === 'trending' && (
-        <h1 className={styles.search__recommend}>
-          {recommend_text[recommendIdx.current]}
-        </h1>
-        )}
-        <label title="검색어 입력" className={styles['search-bar']} htmlFor="searchBarInput">
-          <input className={styles['search-bar__input']} id="searchBarInput" onFocus={changeSearchMode} onBlur={changeTrendingMode} placeholder="검색어를 입력해주세요" value={text} onChange={handleText} />
-          <LensIcon title="검색" className={styles['search-bar__icon']} />
-        </label>
-        {mode === 'trending' && (
-        <RollingBanner />
-        )}
+        <Navigation />
+        {mode === 'trending' && <Recommend />}
+        <SearchInput
+          onFocus={changeSearchMode}
+          onBlur={changeTrendingMode}
+          onChange={handleText}
+          text={text}
+        />
+        {mode === 'trending' && (<RollingBanner />)}
       </section>
-      <ul className={cn({
-        [styles['search-query-list']]: true,
-        [styles['search-query-list--hidden']]: mode === 'trending',
-      })}
-      >
-        {/* 이 부분은 검색창 리스트 필터링 확인을 위해서 임의로 목업 데이터 사용 */}
-        {list?.filter((item) => item.title.includes(text)).length === 0
-        && (
-        <div className={styles['search-query-list__text--not-found']}>
-          해당
-          {` ${text} `}
-          관련 음식점/게시물을 찾을 수 없습니다.
-        </div>
-        )}
-        {text === '' ? null : list?.filter((item) => item.title.includes(text)).map((item) => <li key={item.title} className={styles['search-query-list__item']}>{item.title}</li>)}
-      </ul>
+      <Suggestion mode={mode} list={list} text={text} />
     </div>
   );
 }
