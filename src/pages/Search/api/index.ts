1eb716b791b86044f38f22fb8190e69eac178820
jaewoogwak <jaewoo010207@gmail.com>
2022-11-01 22:54:08 +0900

    feat: api 추가

--- src/pages/Search/api/index.ts
+++ src/pages/Search/api/index.ts
@@ -1,7 +1,22 @@
-import { SearchQueryParams } from './search/entity';
+import { useQuery } from 'react-query';
 import searchApi from './search/searchApiClient';
+// import searchApi from './search/searchApiClient';
 
-// 검색창에서 쿼리에 대한 리스트 받아오기
-const querySeachText = ({ searchQuery } : SearchQueryParams) => searchApi.get(`/search/${searchQuery}`);
+export function useTrendingQuery() {
+  const {
+    isLoading, isError, data,
+  } = useQuery('trending', () => searchApi.get('/trending'));
+  const trendings = data?.data.trendings;
+  return {
+    isLoading, isError, trendings,
+  };
+}
 
-export default querySeachText;
+export function useShopQuery(place_id: string) {
+  const {
+    isLoading, isError, data,
+  } = useQuery('shop', () => searchApi.get(`/shop?place_id=${place_id}`));
+  return {
+    isLoading, isError, data,
+  };
+}
--- src/pages/Search/api/search/searchApiClient.ts
+++ src/pages/Search/api/search/searchApiClient.ts
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
 const API_PATH = process.env.REACT_APP_API_PATH!;
+const AUTH_TOKEN = sessionStorage.getItem('accessToken') || '';
 
 const searchApi = axios.create({
-  baseURL: `${API_PATH}/search`,
+  baseURL: `${API_PATH}:443`,
+  headers: {
+    Authorization: `Bearer ${AUTH_TOKEN}`,
+  },
 });
+searchApi.defaults.headers.common.Authorization = `Bearer ${AUTH_TOKEN}`;
 
 export default searchApi;
