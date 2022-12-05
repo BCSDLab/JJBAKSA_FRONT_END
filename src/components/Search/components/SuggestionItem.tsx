aa210f55ea004db057f06d7615e8935abbfc0678
jaewoogwak <jaewoo010207@gmail.com>
2022-12-05 23:36:41 +0900

    refactor: 관련 검색어 타입 선언 추가

--- src/components/Search/components/SuggestionItem.tsx
+++ src/components/Search/components/SuggestionItem.tsx
@@ -1,7 +1,15 @@
 import { Link } from 'react-router-dom';
 import styles from 'pages/Search/Search.module.scss';
 
-function SuggestionItem({ item } : any) {
+interface Props {
+  item: Item
+}
+
+interface Item {
+  title: string
+}
+
+function SuggestionItem({ item }: Props) {
   return (
     <Link to={`/search/${item.title}`} style={{ color: 'black', textDecoration: 'none' }}>
       <li className={styles['search-query-list__item']}>{item.title}</li>
