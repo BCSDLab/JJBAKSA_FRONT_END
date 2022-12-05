2271fe316af36b198758300d68878f0128c8384e
jaewoogwak <jaewoo010207@gmail.com>
2022-12-05 22:09:25 +0900

    refactor: 관련 검색어 리스트 예시 데이터 추가

--- src/components/Search/components/Suggestion.tsx
+++ src/components/Search/components/Suggestion.tsx
@@ -4,7 +4,12 @@ import MODE from '../static/mode';
 import SuggestionItem from './SuggestionItem';
 import suggestion from '../static/suggestion';
 
-function Suggestion({ mode, list, text } : any) {
+interface Props {
+  mode: string,
+  text: string,
+}
+
+function Suggestion({ mode, text }: Props) {
   return (
     <div className={styles.search}>
       <ul className={cn({
@@ -12,17 +17,8 @@ function Suggestion({ mode, list, text } : any) {
         [styles['search-query-list--hidden']]: mode === MODE.trending,
       })}
       >
-        {/* 이 부분은 검색창 리스트 필터링 확인을 위해서 임의로 목업 데이터 사용 */}
-        {list?.filter((item: any) => item.title.includes(text)).length === 0
-        && (
-        <div className={styles['search-query-list__text--not-found']}>
-          해당
-          {` ${text} `}
-          관련 음식점/게시물을 찾을 수 없습니다.
-        </div>
-        )}
-        {text === '' ? null : suggestion.map((item : any) => (
-          <SuggestionItem item={item} />
+        {text === '' ? null : suggestion.map((item) => (
+          <SuggestionItem key={item.id} item={item} />
         ))}
       </ul>
     </div>
