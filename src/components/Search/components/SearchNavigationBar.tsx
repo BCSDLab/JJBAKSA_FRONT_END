6dfc70c49b65074beed11740b9ce9d259adc0ac8
jaewoogwak <jaewoo010207@gmail.com>
2022-12-05 23:32:47 +0900

    style: 불필요한 주석 제거

--- src/components/Search/components/SearchNavigationBar.tsx
+++ src/components/Search/components/SearchNavigationBar.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from 'pages/Search/Search.module.scss';
 
-// eslint-disable-next-line max-len
 export default function SearchNavigationBar(props : React.HTMLAttributes<HTMLElement>) {
   return (
     <nav className={styles['search-nav']} {...props} />
