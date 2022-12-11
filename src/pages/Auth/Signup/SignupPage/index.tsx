19a4d5124d45c2c26c4144673a10f6e90772fb91
WonBeen Choi <zad1264@naver.com>
2022-12-11 23:04:05 +0900

    chore: sha256 미사용에 맞춰 라이브러리, 코드 제거

--- src/pages/Auth/Signup/SignupPage/index.tsx
+++ src/pages/Auth/Signup/SignupPage/index.tsx
@@ -3,6 +3,7 @@ import { FormProvider, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import AuthTitle from 'components/Auth/AuthTitle';
 import Copyright from 'components/Auth/Copyright';
+import { register } from 'api/user';
 import styles from './SignUp.module.scss';
 import useRouteCheck from '../hooks/useRouteCheck';
 import { SignUpFormData } from './entity';
@@ -11,9 +12,22 @@ import EmailInput from './components/EmailInput';
 import PasswordInput from './components/PasswordInput';
 import PasswordCheckInput from './components/PasswordCheckInput';
 
+const useSignUp = () => {
+  const signup = (form: SignUpFormData) => {
+    register({
+      account: form.id,
+      email: `${form.email}@${form.emailDomain}`,
+      password: form.password,
+    });
+  };
+
+  return signup;
+};
+
 export default function SignUpForm() {
   useRouteCheck('termsCheck', '/terms-of-service');
   const navigate = useNavigate();
+  const signup = useSignUp();
 
   const methods = useForm<SignUpFormData>({
     mode: 'onChange',
@@ -33,7 +47,7 @@ export default function SignUpForm() {
 
   const clickSubmit = () => {
     if (isDirty && isValid) {
-      navigate('/signup/complete', { state: { signUpCheck: true }, replace: true });
+      // navigate('/signup/complete', { state: { signUpCheck: true }, replace: true });
     } else {
       navigate('');
     }
@@ -47,7 +61,7 @@ export default function SignUpForm() {
           <form
             className={styles.form}
             // form 제출 api 호출
-            onSubmit={handleSubmit((res) => res)}
+            onSubmit={handleSubmit(signup)}
           >
             <div className={styles.form__title}>회원가입</div>
 
