f1849ff43777bab5490b70da035f21108327e6df
jaewoogwak <jaewoo010207@gmail.com>
2022-12-14 17:20:35 +0900

    style: 불필요한 파일 삭제

--- src/pages/Search/components/SearchNavigationBar.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from '../Search.module.scss';
-
-export default function SearchNavigationBar(props : React.HTMLAttributes<HTMLElement>) {
-  return (
-    <nav className={styles['search-nav']} {...props} />
-  );
-}
