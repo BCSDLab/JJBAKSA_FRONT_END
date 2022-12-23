36b305ce25d703416470c55b7fe894a8b5a4caa2
jaewoogwak <jaewoo010207@gmail.com>
2022-12-23 09:55:57 +0900

    refactor: Fetcher 반환 타입 정의
    Fetcher 반환 타입을 정의해줌으로써 useQuery의 반환값을 제거

--- src/api/search/entity.ts
+++ src/api/search/entity.ts
@@ -7,6 +7,21 @@ export interface ShopsParams {
   location: Coords;
 }
 
+export interface FetchShopsResponse {
+  content: Shop[];
+}
+
+interface Shop {
+  address: string,
+  dist: 0,
+  placeId: string,
+  placeName: string,
+  score: number,
+  shopId: number,
+  x: string,
+  y: string,
+}
+
 export interface Coords {
   latitude: number,
   longitude: number
