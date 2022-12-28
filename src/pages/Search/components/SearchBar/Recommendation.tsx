039ec5c5b27d4f3e18a16e72140a73236ed9a9dd
jaewoogwak <jaewoo010207@gmail.com>
2022-12-28 21:24:10 +0900

    refactor: 추천문구 랜덤넘버 값 생성 방법 변경

--- src/pages/Search/components/SearchBar/Recommendation.tsx
+++ src/pages/Search/components/SearchBar/Recommendation.tsx
@@ -1,12 +1,12 @@
-import { useRef } from 'react';
 import recommend_text from 'pages/Search/static/recommend';
 import styles from 'pages/Search/components/SearchBar/SearchBar.module.scss';
 
 export default function Recommendation() {
-  const recommendIdx = useRef(new Date().getSeconds() % recommend_text.length);
+  const recommendIdx = Math.ceil(Math.random() * 10) % recommend_text.length;
+
   return (
     <h1 className={styles.search__recommend}>
-      {recommend_text[recommendIdx.current]}
+      {recommend_text[recommendIdx]}
     </h1>
   );
 }
