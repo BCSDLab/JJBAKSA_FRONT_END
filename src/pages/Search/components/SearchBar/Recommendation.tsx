2c183fcdd44d4dbc0c422a92dddd87359e718c0d
jaewoogwak <jaewoo010207@gmail.com>
2022-12-29 23:38:57 +0900

    refactor: 상수 이름 UPPER_CASE로 변경

--- src/pages/Search/components/SearchBar/Recommendation.tsx
+++ src/pages/Search/components/SearchBar/Recommendation.tsx
@@ -1,13 +1,13 @@
-import recommend_text from 'pages/Search/static/recommend';
+import RECOMMEND_TEXT from 'pages/Search/static/recommend';
 import styles from 'pages/Search/components/SearchBar/SearchBar.module.scss';
 import { useRef } from 'react';
 
 export default function Recommendation() {
-  const recommendIdx = useRef(Math.ceil(Math.random() * 10) % recommend_text.length);
+  const recommendIdx = useRef(Math.floor(Math.random() * RECOMMEND_TEXT.length));
 
   return (
     <h1 className={styles.search__recommend}>
-      {recommend_text[recommendIdx.current]}
+      {RECOMMEND_TEXT[recommendIdx.current]}
     </h1>
   );
 }
