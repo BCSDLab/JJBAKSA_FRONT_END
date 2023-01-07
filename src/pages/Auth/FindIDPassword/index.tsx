d49810ea4891306ae0ef8f7674ffb4b469374b8a
채승윤 <thslr57@naver.com>
2023-01-07 17:50:03 +0900

    chore: 기존에 작성된 정규식을 가져다가 사용함

--- src/pages/Auth/FindIDPassword/index.tsx
+++ src/pages/Auth/FindIDPassword/index.tsx
@@ -3,11 +3,10 @@ import { useNavigate } from 'react-router-dom';
 import error from 'assets/svg/auth/error.svg';
 import PreviousButton from 'components/PreviousButton/PreviousButton';
 import cn from 'utils/ts/classNames';
+import { EMAIL_REGEXP } from 'components/Auth/static/Regexp';
 import { sendEmail } from './api';
 import { FindProp, EmailInfo } from './entity';
 import style from './index.module.scss';
-
-const EMAILPATTERN = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i; // 이메일 형식 유효성 검사 패턴
 
 export default function FindIdPassword({ find }: FindProp): JSX.Element {
   const {
@@ -74,7 +73,7 @@ export default function FindIdPassword({ find }: FindProp): JSX.Element {
               {...register('email', {
                 required: 'email을 입력해주세요',
                 pattern: {
-                  value: EMAILPATTERN,
+                  value: EMAIL_REGEXP,
                   message: '올바른 email 형식이 아닙니다.',
                 },
               })}
