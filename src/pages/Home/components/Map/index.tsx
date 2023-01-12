4b342c8a6539bd49188bf8895f6849e5978ba443
kimeodml <kimeodml@gmail.com>
2023-01-12 20:11:35 +0900

    fix: 반복문 forEach로 수정, 함수명 변경

--- src/pages/Home/components/Map/index.tsx
+++ src/pages/Home/components/Map/index.tsx
@@ -19,6 +19,30 @@ export default function Map(): JSX.Element {
   const markerRef = useRef<any | null>(null);
   const selectedMarker = useRef<any | null>(null);
 
+  const markerHighlightEvent = (markerCur:any, item:Props) => {
+    naver.maps.Event.addListener(markerCur, 'click', () => {
+      if (selectedMarker.current) {
+        selectedMarker.current.setOptions({
+          icon: {
+            content: markerHtml(defaultImage, selectedMarker.current.title),
+            size: new naver.maps.Size(50, 52),
+            anchor: new naver.maps.Point(25, 26),
+          },
+        });
+      }
+      markerCur.setOptions({
+        icon: {
+          content: clickedMarkerHtml(defaultImage, item.placeName),
+          size: new naver.maps.Size(50, 52),
+          anchor: new naver.maps.Point(25, 26),
+        },
+      });
+      selectedMarker.current = markerCur;
+      const mapLatLng = new naver.maps.LatLng(item.latitude, item.longitude);
+      mapRef.current.panTo(mapLatLng, 'easeInOutCubic');
+    });
+  };
+
   useEffect(() => {
     if (typeof location !== 'undefined') {
       mapRef.current = new naver.maps.Map('map', {
@@ -33,42 +57,21 @@ export default function Map(): JSX.Element {
   }, [location]);
 
   useEffect(() => {
-    const markerClickEvent = (markerCur:any, item:Props) => {
-      naver.maps.Event.addListener(markerCur, 'click', () => {
-        if (selectedMarker.current) {
-          selectedMarker.current.setOptions({
-            icon: {
-              content: markerHtml(defaultImage, selectedMarker.current.title),
-              size: new naver.maps.Size(50, 52),
-              anchor: new naver.maps.Point(25, 26),
-            },
-          });
-        }
-        markerCur.setOptions({
+    marker.forEach((item:Props) => {
+      if (mapRef.current) {
+        markerRef.current = new naver.maps.Marker({
+          position: new naver.maps.LatLng(item.latitude, item.longitude),
+          title: item.placeName,
+          map: mapRef.current,
           icon: {
-            content: clickedMarkerHtml(defaultImage, item.placeName),
+            content: markerHtml(defaultImage, item.placeName),
             size: new naver.maps.Size(50, 52),
             anchor: new naver.maps.Point(25, 26),
           },
         });
-        selectedMarker.current = markerCur;
-        const mapLatLng = new naver.maps.LatLng(item.latitude, item.longitude);
-        mapRef.current.panTo(mapLatLng, 'easeInOutCubic');
-      });
-    };
-    for (let i = 0; i < marker.length; i += 1) {
-      markerRef.current = new naver.maps.Marker({
-        position: new naver.maps.LatLng(marker[i].latitude, marker[i].longitude),
-        title: marker[i].placeName,
-        map: mapRef.current,
-        icon: {
-          content: markerHtml(defaultImage, marker[i].placeName),
-          size: new naver.maps.Size(50, 52),
-          anchor: new naver.maps.Point(25, 26),
-        },
-      });
-      markerClickEvent(markerRef.current, marker[i]);
-    }
+        markerHighlightEvent(markerRef.current, item);
+      }
+    });
   }, [location]);
   return (
     <div>
