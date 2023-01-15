95c6be2c7f7d2d6397dbc1c05a73eda0f8f6b71f
kimeodml <kimeodml@gmail.com>
2023-01-15 18:02:13 +0900

    feat: PC 필터 추가

--- src/pages/Home/components/OptionButtons/FilterMenu.tsx
+++ src/pages/Home/components/OptionButtons/FilterMenu.tsx
@@ -1,13 +1,17 @@
-import { ReactComponent as MenuIcon } from 'assets/svg/home/mobile-menu.svg';
+import { ReactComponent as MenuIcon } from 'assets/svg/home/menu.svg';
 import { ReactComponent as MapIcon } from 'assets/svg/home/mobile-map.svg';
 import { ReactComponent as FriendIcon } from 'assets/svg/home/mobile-friends.svg';
 import { ReactComponent as BookMarkIcon } from 'assets/svg/home/mobile-bookmark.svg';
+import { ReactComponent as PencilIcon } from 'assets/svg/home/pencil.svg';
 import useBooleanState from 'utils/hooks/useBooleanState';
 import cn from 'utils/ts/classNames';
+import { Link } from 'react-router-dom';
+import useMediaQuery from 'utils/hooks/useMediaQuery';
 import styles from './OptionButtons.module.scss';
 
 export default function FilterMenu(): JSX.Element {
   const [visible,,, toggle] = useBooleanState(false);
+  const { isMobile } = useMediaQuery();
   return (
     <div
       className={cn({
@@ -16,51 +20,55 @@ export default function FilterMenu(): JSX.Element {
       })}
     >
       <div>
-        <button
-          type="button"
-          className={cn({
-            [styles.filter__button]: true,
-            [styles['filter__button--list']]: true,
-          })}
-          onClick={toggle}
-        >
-          <MenuIcon />
-        </button>
+        <ul className={styles.filter}>
+          <li>
+            <button
+              type="button"
+              className={styles.filter__button}
+              onClick={toggle}
+            >
+              <MenuIcon />
+              <span>필터</span>
+            </button>
+          </li>
+          {!isMobile && (
+            <li>
+              <Link to="/post" className={styles.filter__button}>
+                <PencilIcon />
+                <span>글쓰기</span>
+              </Link>
+            </li>
+          )}
+        </ul>
       </div>
       <div className={styles['slide-filter-list']}>
         <ul>
           <li>
             <button
               type="button"
-              className={cn({
-                [styles['slide-filter-list__button']]: true,
-              })}
+              className={styles['slide-filter-list__button']}
             >
               가까운 음식점 찾기
             </button>
-            <span><MapIcon /></span>
+            <span className={styles['slide-filter-list__icon']}><MapIcon /></span>
           </li>
           <li>
             <button
               type="button"
-              className={cn({
-                [styles['slide-filter-list__button']]: true,
-              })}
+              className={styles['slide-filter-list__button']}
             >
               친구 음식점 찾기
             </button>
-            <span><FriendIcon /></span>
+            <span className={styles['slide-filter-list__icon']}><FriendIcon /></span>
           </li>
           <li>
             <button
               type="button"
-              className={cn({
-                [styles['slide-filter-list__button']]: true,
-              })}
+              className={styles['slide-filter-list__button']}
             >
               북마크 음식점 찾기
             </button>
-            <span><BookMarkIcon /></span>
+            <span className={styles['slide-filter-list__icon']}><BookMarkIcon /></span>
           </li>
         </ul>
       </div>
--- src/pages/Home/components/OptionButtons/index.tsx
+++ src/pages/Home/components/OptionButtons/index.tsx
@@ -1,30 +1,10 @@
-import { ReactComponent as MenuIcon } from 'assets/svg/home/menu.svg';
-import { ReactComponent as PencilIcon } from 'assets/svg/home/pencil.svg';
-import { Link } from 'react-router-dom';
-import useMediaQuery from 'utils/hooks/useMediaQuery';
 import FilterMenu from './FilterMenu';
 import styles from './OptionButtons.module.scss';
 
 export default function OptionButtons(): JSX.Element {
-  const { isMobile } = useMediaQuery();
   return (
-    <div>
-      { isMobile ? <FilterMenu /> : (
-        <ul className={styles.button}>
-          <li>
-            <button type="button" className={styles.button__text}>
-              <MenuIcon />
-              <span>필터</span>
-            </button>
-          </li>
-          <li>
-            <Link to="/post" className={styles.button__text}>
-              <PencilIcon />
-              <span>글쓰기</span>
-            </Link>
-          </li>
-        </ul>
-      )}
+    <div className={styles.button}>
+      <FilterMenu />
     </div>
   );
 }
