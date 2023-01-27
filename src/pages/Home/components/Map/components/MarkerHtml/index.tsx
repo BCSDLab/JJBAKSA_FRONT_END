86c9d4097844012c00e477ac500133523c2faf8d
kimeodml <kimeodml@gmail.com>
2023-01-27 19:25:42 +0900

    style: 마커 스타일 수정

--- src/pages/Home/components/Map/components/MarkerHtml/index.tsx
+++ src/pages/Home/components/Map/components/MarkerHtml/index.tsx
@@ -2,34 +2,32 @@ import styles from './MarkerHtml.module.scss';
 
 export function MarkerHtml(defaultImg:string, name:string) {
   return `
-    <div class=${styles.bubble}>
-      <div class=${styles.bubble__text}>
-        <img src=${defaultImg} alt="음식 이미지" />
-        <span>
+    <div class=${styles.marker}>
+      <div class=${styles.bubble}>
+        <div class=${styles.bubble__photo}>
+          <img src=${defaultImg} alt="음식 이미지" />
+        </div>
+        <div class=${styles.bubble__name}>
           ${name}
-        </span>
+        </div>
       </div>
-      <div class=${styles.triangle}>
-        <div class=${styles.triangle__background} />
-        <div class=${styles.triangle__color} />
-      </div>
+      <div class=${styles.dummy} />
     </div>
   `;
 }
 
 export function ClickedMarkerHtml(defaultImg:string, name:string) {
   return `
-    <div class=${styles['bubble--clicked']}>
-      <div class=${styles.bubble__text}>
-        <img src=${defaultImg} alt="음식 이미지" />
-        <span>
+    <div class=${styles['marker--clicked']}>
+      <div class=${styles['bubble--clicked']}>
+        <div class=${styles.bubble__photo}>
+          <img src=${defaultImg} alt="음식 이미지" />
+        </div>
+        <div class=${styles.bubble__name}>
           ${name}
-        </span>
+        </div>
       </div>
-      <div class=${styles.triangle}>
-        <div class=${styles.triangle__background} />
-        <div class=${styles['triangle__color--clicked']} />
-      </div>
+      <div class=${styles['dummy--clicked']} />
     </div>
   `;
 }
