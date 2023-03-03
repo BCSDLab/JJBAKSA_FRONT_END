9b6c5e78fb9577a86500b7df3eaa95f0420744d5
Jaewoo Gwak <jaewoo010207@gmail.com>
2023-03-03 16:22:54 +0900

    Feature/#17 PC버전 검색 결과창 구현 (#54)
    * feat: PC 버전 검색 페이지에 TopNavigation 추가
    
    * feat: PC 버전 검색 결과 아이템 UI 구현
    
    * feat: 검색 결과 아이템들을 두 줄로 표시하도록 구현
    
    * style: 아이템 크기 수정
    
    * feat: 체크 박스 추가
    
    * fix: 상단 바가 두 개 나타나던 현상 해결
    
    * style: PC버전 검색 결과창 너비 수정
    
    * style: lint 적용
    
    * style: lint 에러 해결
    
    * feat: 아이템에 마우스 커서 올리면 배경 색상 변경 적용
    
    * style: 영업 종료 스타일 효과 추가
    
    * style: lint 적용
    
    * fix: 모바일에서 아이템 배경 색상이 변경되어 나오는 현상 해결
    
    * refactor: 임시 데이터를 관리하기 위한mock함수 추가
    
    * feat: 전화 바로 걸기 기능 추가
    
    * feat: 카드 클릭 시 링크 이동 추가
    
    * fix: 전화번호 클릭 시 전화 연결이 작동하지 않는 현상 해결
    
    전화번호 링크를 올바르게 수정하고,
    전화번호 클릭 시 카드 호버 이벤트 전파를 막아서 해결하였습니다.
    
    * fix: 이미지가 존재하지 않을 때 기본 이미지를 보여주도록 변경

--- src/pages/Search/components/NavigationBar/index.tsx
+++ src/pages/Search/components/NavigationBar/index.tsx
@@ -1,16 +1,22 @@
 import PreviousButton from 'components/PreviousButton/PreviousButton';
 import styles from 'pages/Search/components/NavigationBar/NavigationBar.module.scss';
+import useMediaQuery from 'utils/hooks/useMediaQuery';
 
 interface Props {
   keyword?: string;
 }
 
 export default function NavigationBar({ keyword = '검색' }: Props) {
+  const { isMobile } = useMediaQuery();
+
   return (
     <nav className={styles['search-nav']}>
-      <div className={styles['search-nav__button']}>
-        <PreviousButton />
-      </div>
+      {isMobile
+        && (
+        <div className={styles['search-nav__button']}>
+          <PreviousButton />
+        </div>
+        )}
       <h1 className={styles['search-nav__text']}>{keyword}</h1>
     </nav>
   );
---
+++ src/pages/SearchDetails/static/mockup.ts
@@ -0,0 +1,27 @@
+import defaultImage from 'assets/images/search/default-image.png';
+
+export interface GetItemResponse {
+  imageAlt: string,
+  phoneNumber: string,
+  defaultImage: string,
+  image: undefined,
+}
+
+export const getMockItem = ():GetItemResponse => {
+  const data = {
+    imageAlt: '가게 이미지 없음',
+    phoneNumber: '010-0000-0000',
+    defaultImage,
+    image: undefined,
+  };
+  return data;
+};
+
+export const SHOPS = {
+  shopName: '카페',
+  address: '서울특별시 마포구 서교동',
+  phoneNumber: '010-0000-0000',
+  status: '영업 중',
+  closing: '- 21:00에 영업 종료',
+  distance: '내 위치로부터 23m',
+};
