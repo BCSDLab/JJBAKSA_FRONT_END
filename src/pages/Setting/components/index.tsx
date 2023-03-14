0b6249dfd4aa439b75a5421dc465145e0ea89911
김혜준 <alice5855@naver.com>
2023-03-15 01:39:47 +0900

    style: 설정창 디자인 수정

--- src/pages/Setting/components/index.tsx
+++ src/pages/Setting/components/index.tsx
@@ -9,59 +9,60 @@ export default function Setting(): JSX.Element {
     <div className={styles.container}>
 
       <div className={styles.head}>
-        <div className={styles['title__navigation--back']}>
+        <div className={styles['head__left-arrow']}>
           <Link to="/"><ArrowLeft /></Link>
         </div>
-        <div className={styles.title__Setting}>설정</div>
+        <div className={styles.title}>설정</div>
       </div>
 
       <div className={styles.account}>
-        <div className={styles['account-setting']}>계정 관리</div>
-        <div className={styles['account-setting__id']}>
-          <div className={styles['account-Setting__id--change']}>아이디 변경</div>
-          <div className={styles['id-id']}>fgnsdbcxb</div>
-          <div className={styles['account-setting__id--move']}>
+        <div className={styles['sub-title']}>계정 관리</div>
+        <div className={styles.block}>
+          <div className={styles['account-text']}>아이디 변경</div>
+          <div className={styles['current-text']}>fgnsdbcxb</div>
+          <div className={styles['id__right-arrow']}>
             <Link to="/"><ArrowRight /></Link>
           </div>
         </div>
-        <div className={styles['account-setting__password']}>
-          <div className={styles['account-setting__password--change']}>비밀번호 변경 </div>
-          <div className={styles['account-setting__password--move']}>
+        <div className={styles.block}>
+          <div className={styles['account-text']}>비밀번호 변경 </div>
+          <div className={styles['password__right-arrow']}>
             <Link to="/"><ArrowRight /></Link>
           </div>
         </div>
-        <div className={styles['account-setting__password__personal-information']}>
-          <div className={styles['account-Setting__personal-information--manual']}>개인정보 이용방침</div>
-          <div className={styles['account-setting__password__personal-information--move']}>
+        <div className={styles.block}>
+          <div className={styles['account-text']}>개인정보 이용방침</div>
+          <div className={styles.policy__announcement}>
             <Link to="/"><Move /></Link>
           </div>
         </div>
       </div>
 
-      <div className={styles['service-block']}>
-        <div className={styles['service-title']}>서비스</div>
-        <div className={styles['service-block__announcement']}>
-          <div className={styles['service-block__announcement--title']}>공지사항</div>
-          <div className={styles['service-block__announcement--title--move']}>
+      <div className={styles.service}>
+        <div className={styles['sub-title']}>서비스</div>
+        <div className={styles.block}>
+          <div className={styles['service-text']}>공지사항</div>
+          <div className={styles.service__announcement}>
             <Link to="/"><Move /></Link>
           </div>
         </div>
-        <div className={styles['service-block__inquiry']}>
-          <div className={styles['service-block__inquiry--title']}>문의하기</div>
-          <div className={styles['service-block__inquiry--move']}>
+
+        <div className={styles.block}>
+          <div className={styles['service-text']}>문의하기</div>
+          <div className={styles.service__announcement}>
             <Link to="/"><Move /></Link>
           </div>
         </div>
-        <div className={styles['service-block__app-version']}>
-          <div className={styles['service-block__app-version--title ']}>앱 버전</div>
-          <div className={styles['service-block__app-version--imformation']}>
+        <div className={styles.block}>
+          <div className={styles['service-text']}>앱 버전</div>
+          <div className={styles['app-version--imformation']}>
             현재 1.2.0 / 최신 1.2.0
           </div>
         </div>
       </div>
       <div className={styles.bottom}>
         <div className={styles['bottom__log-out']}>로그아웃</div>
-        <div className={styles['bottom__delete-account']}>탈퇴하기기</div>
+        <div className={styles['bottom__delete-account']}>탈퇴하기</div>
       </div>
     </div>
   );
