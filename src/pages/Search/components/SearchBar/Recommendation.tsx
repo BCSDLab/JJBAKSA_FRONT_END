0e9f864e01eccb2879b97c753c25cf8f5617827d
dbshaejin <dbshaejin@gmail.com>
2023-03-15 19:08:53 +0900

    style: 검색창 UI 수정, 로그인 버튼 UI수정

--- src/pages/Search/components/SearchBar/Recommendation.tsx
+++ src/pages/Search/components/SearchBar/Recommendation.tsx
@@ -5,9 +5,14 @@ import { useRef } from 'react';
 export default function Recommendation() {
   const recommendIdx = useRef(Math.floor(Math.random() * RECOMMEND_TEXT.length));
 
+  const recommendationSentence = RECOMMEND_TEXT[recommendIdx.current];
+
   return (
-    <h1 className={styles.search__recommend}>
-      {RECOMMEND_TEXT[recommendIdx.current]}
-    </h1>
+    <h1
+      className={styles.search__recommend}
+      dangerouslySetInnerHTML={{ __html: recommendationSentence[0] }}
+    />
   );
 }
+
+// className={styles.point-recommend-font-color}
--- src/pages/Search/static/recommend.ts
+++ src/pages/Search/static/recommend.ts
@@ -1,7 +1,7 @@
 const RECOMMEND_TEXT = [
-  '오늘은\n어떤 음식이 땡기나요?',
-  '기름진 음식이\n그리운 날!',
-  '추억의 장소를\n검색을 통해 찾아보세요!',
+  ['오늘은\n<strong>어떤 음식</strong>이 땡기나요?'],
+  ['<strong>기름진 음식</strong>이\n그리운 날!'],
+  ['<strong>추억의 장소</strong>를\n검색을 통해 찾아보세요!'],
 ];
 
 export default RECOMMEND_TEXT;
