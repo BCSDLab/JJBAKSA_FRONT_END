576ae4ab3b329401dc372a541553ba77d53b4bb3
채승윤 <thslr57@naver.com>
2023-03-18 13:21:38 +0900

    feat: follow 관련 api 추가

---
+++ src/api/follow/entity.ts
@@ -0,0 +1,26 @@
+export interface FollowListParams {
+  cursor: string,
+  pageSize: number;
+}
+
+export interface FollowResponse {
+  account: string,
+  email: string,
+  id: number,
+  nickname: string,
+  oauthType: string;
+}
+
+export interface CheckReceivedFollowParams {
+  page: number,
+  pageSize: number;
+}
+
+export interface CheckSendedFollowParams {
+  page: number,
+  pageSize: number;
+}
+
+export interface FollowRequestParams {
+  userAccount: string;
+}
