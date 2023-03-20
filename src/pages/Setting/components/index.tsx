ff8004694c0a4f4c715436803a8ad3da54b49a4c
hyejun <alice5855@naver.com>
2023-03-20 17:10:18 +0900

    style: scss bem 형식으로 변경

--- src/pages/Setting/components/index.tsx
+++ src/pages/Setting/components/index.tsx
@@ -10,51 +10,60 @@ export default function Setting(): JSX.Element {
   const auth = useAuth();
   return (
     <div className={styles.container}>
-      <div className={styles.head}>
-
-        <div className={styles['head__left-arrow']}>
+      <div className={styles.header}>
+        <div className={styles['header__left-arrow']}>
           <PreviousButton />
         </div>
-        <div className={styles.title}>설정</div>
+        <div className={styles.header__title}>설정</div>
       </div>
       <div className={styles.account}>
-        <div className={styles['sub-title']}>계정 관리</div>
-        <div className={styles.block}>
-          <div className={styles['account-text']}>아이디 변경</div>
-          <div className={styles['current-text']}>{auth?.account}</div>
-          <div className={styles['id__right-arrow']}>
-            <Link to="/"><ArrowRight /></Link>
+        <div className={styles['account__sub-title']}>계정 관리</div>
+        <Link to="/" className={styles.link}>
+          <div className={styles.account__contents}>
+            <div className={styles.account__text}>아이디 변경</div>
+            <div className={styles.account__text}>{auth?.account}</div>
+            <div className={styles['account__right-arrow']}>
+              <ArrowRight />
+            </div>
           </div>
-        </div>
-        <div className={styles.block}>
-          <div className={styles['account-text']}>비밀번호 변경 </div>
-          <div className={styles['password__right-arrow']}>
-            <Link to="/"><ArrowRight /></Link>
+        </Link>
+        <Link to="/" className={styles.link}>
+          <div className={styles.account__contents}>
+            <div className={styles.account__text}>비밀번호 변경 </div>
+            <div className={styles['account__right-arrow']}>
+              <ArrowRight />
+            </div>
           </div>
-        </div>
-        <div className={styles.block}>
-          <div className={styles['account-text']}>개인정보 이용방침</div>
-          <div className={styles.policy__announcement}>
-            <Link to="/"><Move /></Link>
+        </Link>
+        <Link to="/" className={styles.link}>
+          <div className={styles.account__contents}>
+            <div className={styles.account__text}>개인정보 이용방침</div>
+            <div className={styles.policy__announcement}>
+              <Move />
+            </div>
           </div>
-        </div>
+        </Link>
       </div>
       <div className={styles.service}>
-        <div className={styles['sub-title']}>서비스</div>
-        <div className={styles.block}>
-          <div className={styles['service-text']}>공지사항</div>
-          <div className={styles.service__announcement}>
-            <Link to="/"><Move /></Link>
+        <div className={styles['service__sub-title']}>서비스</div>
+        <Link to="/" className={styles.link}>
+          <div className={styles.service__contents}>
+            <div className={styles.service__text}>공지사항</div>
+            <div className={styles.policy__announcements}>
+              <Move />
+            </div>
           </div>
-        </div>
-        <div className={styles.block}>
-          <div className={styles['service-text']}>문의하기</div>
-          <div className={styles.service__announcement}>
-            <Link to="/"><Move /></Link>
+        </Link>
+        <Link to="/" className={styles.link}>
+          <div className={styles.service__contents}>
+            <div className={styles.service__text}>문의하기</div>
+            <div className={styles.policy__announcement}>
+              <Move />
+            </div>
           </div>
-        </div>
-        <div className={styles.block}>
-          <div className={styles['service-text']}>앱 버전</div>
+        </Link>
+        <div className={styles.service__contents}>
+          <div className={styles.service__text}>앱 버전</div>
           <div className={styles['app-version--imformation']}>
             현재 1.2.0 / 최신 1.2.0
           </div>
