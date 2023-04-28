b3fd50d15e6001500c0d91ee9800e10090104fc1
김혜준 <alice5855@naver.com>
2023-04-29 03:57:49 +0900

    fix: 문의하기 목록 grid형태로 display변경

--- src/pages/Setting/components/Inquiry.tsx
+++ src/pages/Setting/components/Inquiry.tsx
@@ -7,15 +7,17 @@ export default function Inquiry(): JSX.Element {
         <div className={styles.header__title}>문의하기</div>
         <div className={styles['header__sub-title']}>쩝쩝박사에게 궁금한 점이 있나요?</div>
       </div>
-      <hr className={styles['line--first']} />
-      <div className={styles.box}>
-        <div className={styles.box__number}>NO</div>
-        <div className={styles.box__title}>TITLE</div>
-        <div>NAME</div>
-        <div className={styles.box__date}>DATE</div>
-        <div>HIT</div>
+      <div className={styles.body}>
+        <hr className={styles['line--first']} />
+        <div className={styles.box}>
+          <div>NO</div>
+          <div>TITLE</div>
+          <div>NAME</div>
+          <div>DATE</div>
+          <div>HIT</div>
+        </div>
+        <hr className={styles['line--second']} />
       </div>
-      <hr className={styles['line--second']} />
       <div className={styles.footer}>
         <input placeholder="제목 혹은 작성자를 검색해보세요!" className={styles.footer__search} />
         <button type="submit" className={styles['footer__search-button']}>찾기</button>
