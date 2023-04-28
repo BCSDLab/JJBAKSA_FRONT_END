fd6d0f8024e3656da48a25b7189a889b4335ba24
김혜준 <alice5855@naver.com>
2023-04-29 02:39:27 +0900

    fix: 코드 중복 수정

--- src/pages/Setting/components/Inquiry.tsx
+++ src/pages/Setting/components/Inquiry.tsx
@@ -7,7 +7,7 @@ export default function Inquiry(): JSX.Element {
         <div className={styles.header__title}>문의하기</div>
         <div className={styles['header__sub-title']}>쩝쩝박사에게 궁금한 점이 있나요?</div>
       </div>
-      <hr className={styles.mainline} />
+      <hr className={styles['line--first']} />
       <div className={styles.box}>
         <div className={styles.box__number}>NO</div>
         <div className={styles.box__title}>TITLE</div>
@@ -15,9 +15,9 @@ export default function Inquiry(): JSX.Element {
         <div className={styles.box__date}>DATE</div>
         <div>HIT</div>
       </div>
-      <hr className={styles.line} />
+      <hr className={styles['line--second']} />
       <div className={styles.footer}>
-        <input type="text" value="제목 혹은 작성자를 검색해보세요!" className={styles.footer__search} />
+        <input placeholder="제목 혹은 작성자를 검색해보세요!" className={styles.footer__search} />
         <button type="submit" className={styles['footer__search-button']}>찾기</button>
       </div>
     </div>
