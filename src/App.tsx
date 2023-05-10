14e8ba46a32ce7b3d6c8e13d1b3ef8aa0c687e52
김혜준 <alice5855@naver.com>
2023-05-10 15:03:55 +0900

    Squashed commit of the following:
    commit 0cfc638d0f7e555ef1ba3c25dbcf3878dc740770
    Merge: a158ea5 22babc4
    Author: KimDoToRi <60084235+KimKyungYun@users.noreply.github.com>
    Date:   Sun May 7 13:13:07 2023 +0900
    
        Merge pull request #73 from BCSDLab/feature/#70
    
        Feature/#70 탈퇴하기 페이지 제작
    
    commit 22babc4325a2b4234d3376e1f535945be9f61328
    Author: KimKyungYun <60084235+KimKyungYun@users.noreply.github.com>
    Date:   Sun May 7 13:10:42 2023 +0900
    
        fix: 모달 표시방식 수정, navigate삭제
    
    commit 79e06475a799456faa4b5e0b2dfd82742f2487d8
    Author: KimKyungYun <60084235+KimKyungYun@users.noreply.github.com>
    Date:   Thu May 4 11:53:57 2023 +0900
    
        fix: 콜백 호출 수정, 토큰 제거 추가
    
    commit 892c8d2e74504c20601feebf61c3499536d9147d
    Author: KimKyungYun <60084235+KimKyungYun@users.noreply.github.com>
    Date:   Wed May 3 14:08:54 2023 +0900
    
        fix: 설정페이지 폴더명 변경
    
    commit 8b6dc493a5d0117cc77f10d3f004b7102283c436
    Author: KimKyungYun <60084235+KimKyungYun@users.noreply.github.com>
    Date:   Wed May 3 12:02:09 2023 +0900
    
        fix: lint오류 수정
    
    commit ca627dd00d1257a5ed4309f1b5d959a3ff0f3aff
    Author: KimKyungYun <60084235+KimKyungYun@users.noreply.github.com>
    Date:   Wed May 3 10:21:04 2023 +0900
    
        feat: 로그아웃기능 추가
    
    commit 65ac76a14c439158226a523214e42275e1dd55f6
    Author: KimKyungYun <60084235+KimKyungYun@users.noreply.github.com>
    Date:   Wed May 3 00:46:59 2023 +0900
    
        feat: 탈퇴기능, 유저정보 초기화 추가
    
    commit b49f06bbae170efcbd0a69c4746e2f39c5a0417b
    Author: KimKyungYun <60084235+KimKyungYun@users.noreply.github.com>
    Date:   Tue May 2 14:32:12 2023 +0900
    
        feat: 탈퇴완료 modal 제작
    
    commit 65a8954b9ffa8a7eedbc620b8dd8bc0a61fbbf80
    Author: KimKyungYun <60084235+KimKyungYun@users.noreply.github.com>
    Date:   Tue May 2 13:50:53 2023 +0900
    
        feat: 회원탈퇴 페이지 설문박스 제작

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,10 @@ import { Routes, Route } from 'react-router-dom';
 import ChangePassword from 'pages/Auth/FindIDPassword/ChangePassword';
 import { Suspense } from 'react';
 import SearchDetails from 'pages/SearchDetails';
-import Setting from 'pages/Setting';
-import IdChange from 'pages/Setting/components/IdChange';
+import Setting from 'pages/Setting/UserSetting';
+import IdChange from 'pages/Setting/UserSetting/IdChange';
 import AuthRoute from 'components/common/AuthRoute';
+import Withdrawal from 'pages/Setting/Withdrawal';
 import Inquiry from 'pages/Inquiry';
 import Myinquiry from 'pages/Inquiry/Myinquiry';
 
@@ -32,6 +33,7 @@ export default function App(): JSX.Element {
         <Route element={<AuthRoute needAuth redirectRoute="/login" />}>
           <Route path="/setting" element={<Setting />} />
           <Route path="/setting/id-change" element={<IdChange />} />
+          <Route path="/withdrawal" element={<Withdrawal />} />
         </Route>
         <Route element={<AuthRoute needAuth={false} redirectRoute="/" />}>
           <Route path="/login" element={<Login />} />
---
+++ src/pages/Setting/Withdrawal/components/WithdrawalModal.tsx
@@ -0,0 +1,27 @@
+import AuthTitle from 'components/Auth/AuthTitle';
+import AuthDetail from 'components/Auth/AuthDetail';
+import { useClearAuth } from 'store/auth';
+import styles from './WithdrawalModal.module.scss';
+
+export default function WithdrawalModalModal() {
+  const clearAuth = useClearAuth();
+
+  return (
+    <div className={styles.modal}>
+      <div className={styles.modal__popup}>
+        <div className={styles.modal__message}>
+          <button className={styles.modal__close} type="button" onClick={clearAuth}>X</button>
+        </div>
+        <AuthTitle />
+        <AuthDetail name="탈퇴를 완료했습니다." first="다음에 또 봐요! 재가입은 탈퇴 후" second="일주일 후 부터 가능합니다." />
+        <button
+          className={styles.modal__button}
+          type="button"
+          onClick={clearAuth}
+        >
+          닫기
+        </button>
+      </div>
+    </div>
+  );
+}
