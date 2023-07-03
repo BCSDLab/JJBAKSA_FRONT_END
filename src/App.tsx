b8edb04b5347c95b930a39a82b9a31595de00e40
채승윤 <thslr57@naver.com>
2023-07-03 16:15:15 +0900

    feat: 네이버, 카카오 sns 로그인 추가

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,8 @@ import Withdrawal from 'pages/Setting/Withdrawal';
 import Inquiry from 'pages/Inquiry';
 import Myinquiry from 'pages/Inquiry/Myinquiry';
 import Notice from 'pages/Notice';
+import KakaoLogin from 'KakaoLogin';
+import NaverLogin from 'NaverLogin';
 
 export default function App(): JSX.Element {
   return (
@@ -50,6 +52,8 @@ export default function App(): JSX.Element {
           <Route path="/find-password" element={<FindIdPassword type="password" />} />
           <Route path="/find/verify/:type" element={<VerifyField />} />
           <Route path="/find-password/change" element={<ChangePassword />} />
+          <Route path="/login/oauth2/code/kakao" element={<KakaoLogin />} />
+          <Route path="/login/oauth2/code/naver" element={<NaverLogin />} />
         </Route>
       </Routes>
       <Toast />
