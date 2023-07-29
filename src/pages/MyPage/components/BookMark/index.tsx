9170bd74dac439122b4f95e8e19bcdf708304da4
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2023-07-29 20:48:37 +0900

    feat: 북마크 모바일 UI 구현

--- src/pages/MyPage/components/BookMark/index.tsx
+++ src/pages/MyPage/components/BookMark/index.tsx
@@ -1,3 +1,27 @@
+import filledStar from 'assets/svg/mypage/star-filled.svg';
+import defaultImage from 'assets/images/search/default-image.png';
+import DUMMY from '../MyPost/dummy';
+import styles from './BookMark.module.scss';
+
 export default function BookMark() {
-  return <div>북마크</div>;
+  return (
+    <div className={styles.bookmarks}>
+      <span className={styles.bookmarks__total}>{`총 ${DUMMY.length}개의 음식점`}</span>
+      {DUMMY.map(() => (
+        <div className={styles.bookmark}>
+          <div className={styles.bookmark__detail}>
+            <span className={styles['bookmark__detail--name']}>국밥 공을기</span>
+            <span className={styles['bookmark__detail--type']}>중식당</span>
+          </div>
+          <div className={styles['bookmark__star-rate']}>
+            <img src={filledStar} alt="satr-rate" className={styles['bookmark__star-rate--image']} />
+            <span className={styles['bookmark__star-rate--rate']}>4.0</span>
+          </div>
+          <div className={styles['bookmark__store-image']}>
+            <img className={styles['bookmark__store-image--crop']} src={defaultImage} alt="store" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
