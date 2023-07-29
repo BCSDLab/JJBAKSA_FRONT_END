413bc123e41cd5b8bdd2f9b444aefea1a9c97f0a
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2023-07-29 21:22:36 +0900

    feat: 데스크톱 뷰 리뷰, 북마크 없을 경우 UI 구현

--- src/pages/MyPage/components/MyPost/index.tsx
+++ src/pages/MyPage/components/MyPost/index.tsx
@@ -4,6 +4,7 @@ import filledCheckerboard from 'assets/svg/mypage/checkerboard-filled.svg';
 import filledHamberger from 'assets/svg/mypage/hamberger-filled.svg';
 import hamberger from 'assets/svg/mypage/hamberger.svg';
 import useMediaQuery from 'utils/hooks/useMediaQuery';
+import notExist from 'assets/svg/mypage/not-exist.svg';
 import styles from './MyPost.module.scss';
 import CheckerBoard from '../Checkerboard';
 import DUMMY from './dummy';
@@ -15,17 +16,30 @@ export default function MyPost() {
   const { isMobile } = useMediaQuery();
   return (
     <div>
-      <div className={styles['view-options']}>
-        <button type="button" onClick={() => setType('CHECKERBOARD')} className={styles['view-options__option']}>
-          <img src={viewType === 'CHECKERBOARD' ? filledCheckerboard : checkerboard} alt="checkerboard" />
-        </button>
-        <button type="button" onClick={() => setType('HAMBERGER')} className={styles['view-options__option']}>
-          <img src={viewType === 'HAMBERGER' ? filledHamberger : hamberger} alt="checkerboard" />
-        </button>
-      </div>
-      {!isMobile && viewType === 'CHECKERBOARD' && <CheckerBoard posts={DUMMY} />}
-      {!isMobile && viewType === 'HAMBERGER' && <HambergerBoard posts={DUMMY} />}
-      {isMobile && <MobileBoard posts={DUMMY} />}
+      {DUMMY.length !== 0 ? (
+        <>
+          <div className={styles['view-options']}>
+            <button type="button" onClick={() => setType('CHECKERBOARD')} className={styles['view-options__option']}>
+              <img src={viewType === 'CHECKERBOARD' ? filledCheckerboard : checkerboard} alt="checkerboard" />
+            </button>
+            <button type="button" onClick={() => setType('HAMBERGER')} className={styles['view-options__option']}>
+              <img src={viewType === 'HAMBERGER' ? filledHamberger : hamberger} alt="checkerboard" />
+            </button>
+          </div>
+          {!isMobile && viewType === 'CHECKERBOARD' && <CheckerBoard posts={DUMMY} />}
+          {!isMobile && viewType === 'HAMBERGER' && <HambergerBoard posts={DUMMY} />}
+          {isMobile && <MobileBoard posts={DUMMY} />}
+        </>
+      ) : (
+        <div className={styles['not-exist']}>
+          <span className={styles['not-exist__phrase']}>
+            <p>둥록된 리뷰가 없어요.</p>
+            <p>다녀온 음식점의 리뷰를 작성해 보세요!</p>
+          </span>
+          <img src={notExist} alt="not-exist" className={styles['not-exist__image']} />
+        </div>
+      )}
+
     </div>
   );
 }
