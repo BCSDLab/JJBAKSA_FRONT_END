915d75757b737846777c0c4ab50f7127c6a62608
Daeeui Kim <kimeodml@gmail.com>
2023-07-30 15:18:42 +0900

    fix: 사이드바로 디자인 변경

--- src/components/common/SideNavigation/index.tsx
+++ src/components/common/SideNavigation/index.tsx
@@ -1,56 +1,94 @@
-import { Link } from 'react-router-dom';
-import { ReactComponent as LogoIcon } from 'assets/svg/common/favicon.svg';
-import { ReactComponent as ArrowIcon } from 'assets/svg/common/arrow.svg';
+import { ReactComponent as LogoIcon } from 'assets/svg/common/logo.svg';
 import { ReactComponent as SettingIcon } from 'assets/svg/common/setting.svg';
 import { ReactComponent as WriteIcon } from 'assets/svg/common/write.svg';
 import { ReactComponent as MyPageIcon } from 'assets/svg/common/my-page.svg';
+import { ReactComponent as SearchIcon } from 'assets/svg/search/lens.svg';
 import { useAuth } from 'store/auth';
+import cn from 'utils/ts/classNames';
+import useBooleanState from 'utils/hooks/useBooleanState';
+import { Link } from 'react-router-dom';
 import styles from './SideNavigation.module.scss';
 
 export default function TopNavigation(): JSX.Element {
   const auth = useAuth();
+  const [visible, , , toggle] = useBooleanState(false);
 
-  const tabs = [
+  const TABS = [
+    {
+      name: '',
+      icon: <LogoIcon />,
+      link: '/',
+
+    },
+    {
+      name: '검색',
+      icon: <SearchIcon />,
+      link: '/',
+
+    },
+    {
+      name: '글쓰기',
+      icon: <WriteIcon />,
+      link: '/post',
+    },
+    {
+      name: '마이페이지',
+      icon: <MyPageIcon />,
+      link: auth ? '/profile' : '/login',
+    },
     {
       name: '설정',
       icon: <SettingIcon />,
       link: '/setting',
     },
-    {
-      name: '글쓰기',
-      icon: <WriteIcon />,
-      link: '/search',
-    },
-    {
-      name: auth ? '마이페이지' : '로그인',
-      icon: <MyPageIcon />,
-      link: auth ? '/profile' : '/login',
-    },
   ];
 
   return (
-    <nav className={styles['top-navigation']}>
-      <div className={styles['top-navigation__logo']}>
-        <Link to="/" className={styles['top-navigation__logo-link']}>
-          <LogoIcon />
-        </Link>
-      </div>
-
-      <div className={styles['top-navigation__address']}>
-        <span>충청북도 천안시 병천면 충절로 1600</span>
-        <ArrowIcon aria-hidden />
-      </div>
-
-      <ul className={styles['top-navigation__links']}>
-        {tabs.map((tab) => (
-          <li key={tab.name}>
-            <Link to={tab.link} className={styles['top-navigation__link']}>
-              {tab.icon}
-              {tab.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <div>
+      <nav className={styles['side-navigation']}>
+        <ul className={styles['side-navigation__lists']}>
+          {TABS.map((tab) => (
+            <li
+              key={tab.name}
+              className={cn({
+                [styles['side-navigation__list']]: true,
+                [styles['side-navigation__list--clicked']]: tab.name === '검색' && visible,
+              })}
+            >
+              {tab.name === '검색' ? (
+                <button
+                  type="button"
+                  className={cn({
+                    [styles['side-navigation__button']]: true,
+                    [styles['side-navigation__button--show']]: visible,
+                  })}
+                  onClick={toggle}
+                  tabIndex={0}
+                >
+                  <div>{tab.icon}</div>
+                  <div>{tab.name}</div>
+                </button>
+              ) : (
+                <Link to={tab.link} className={styles['side-navigation__link']}>
+                  <div>{tab.icon}</div>
+                  <div>{tab.name}</div>
+                </Link>
+              )}
+            </li>
+          ))}
+        </ul>
+        <ul className={styles['bottom-navigation']}>
+          {auth ? (
+            <li className={styles['bottom-navigation__login']}>
+              <div>사용자</div>
+            </li>
+          ) : (
+            <li className={styles['bottom-navigation__login']}>
+              <Link to="/login" className={styles['bottom-navigation__link']}>로그인</Link>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </div>
   );
 }
