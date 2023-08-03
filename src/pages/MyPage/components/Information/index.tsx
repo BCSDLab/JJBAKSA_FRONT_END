0c84cee33f690c586e61736a7a99679933bf86dd
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2023-08-03 19:16:37 +0900

    feat: 프로필 데이터 api 연동

--- src/pages/MyPage/components/Information/index.tsx
+++ src/pages/MyPage/components/Information/index.tsx
@@ -2,20 +2,43 @@ import defaultImage from 'assets/images/follow/default-image.png';
 // import fixPencil from 'assets/images/mypage/pencil.svg';
 import option from 'assets/svg/mypage/option.svg';
 import useMediaQuery from 'utils/hooks/useMediaQuery';
+import { getMe } from 'api/user';
+import { useEffect, useState } from 'react';
+import { User } from 'api/user/entity';
 import styles from './Information.module.scss';
+
+type Profile = User & {
+  profileImage?: {
+    url: string
+  }
+};
+
+const useMyInfo = () => {
+  const [profile, setProfile] = useState<Profile>();
+  const getMyInfo = async () => {
+    const { data } = await getMe();
+    setProfile(data);
+  };
+  useEffect(() => {
+    getMyInfo();
+  }, []);
+
+  return { profile };
+};
 
 export default function Information() {
   const { isMobile } = useMediaQuery();
+  const { profile } = useMyInfo();
   return (
     <div className={styles.information}>
       <div className={styles.user}>
         <div className={styles.profile}>
-          <img src={defaultImage} alt="profileImage" className={styles.user__image} />
+          <img src={profile?.profileImage ? profile.profileImage.url : defaultImage} alt="profileImage" className={styles.user__image} />
           {/* <img src={fixPencil} alt="changeProfile" className={styles['change--image']} /> */}
         </div>
         <div>
           <span className={styles.user__nickname}>
-            닉네임
+            {profile?.nickname}
           </span>
           {isMobile && (
           <>
@@ -23,13 +46,13 @@ export default function Information() {
             <span className={styles['totals__follower--number']}>팔로워 120</span>
           </>
           )}
-          <span className={styles.user__account}>abcdIdentifier</span>
+          <span className={styles.user__account}>{profile?.account}</span>
         </div>
       </div>
       {!isMobile && (
       <div className={styles.totals}>
         <div className={styles.totals__post}>
-          <span className={styles['totals__post--number']}>11</span>
+          <span className={styles['totals__post--number']}>{profile?.userCountResponse.reviewCount}</span>
           <span className={styles['totals__post--label']}>게시물</span>
         </div>
         <div className={styles.totals__follower}>
