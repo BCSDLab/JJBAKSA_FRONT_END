b2a3fcb9202134d7f4dca030ba0a4f27d126efbf
Daeeui Kim <kimeodml@gmail.com>
2023-08-06 22:50:08 +0900

    feat: 필터 상점 타입 지정

--- src/api/shop/entity.ts
+++ src/api/shop/entity.ts
@@ -1,3 +1,17 @@
+export interface FilterShopsParams {
+  options_friend: 0 | 1;
+  options_nearby: 0 | 1;
+  options_scrap: 0 | 1;
+  location?: Coords;
+}
+
+export interface FilterShopsResponse {
+  geometry?: Coords;
+  name: string;
+  photo: null | string[];
+  placeId: string;
+}
+
 export interface FetchTrendingsResponse {
   trendings: string[]
 }
@@ -27,6 +41,6 @@ interface Shop {
 }
 
 export interface Coords {
-  latitude: number,
-  longitude: number
+  latitude: number | undefined;
+  longitude: number | undefined;
 }
