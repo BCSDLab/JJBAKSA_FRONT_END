5c538172d619a89eccb44025c9ca7d1977b1f587
Daeeui Kim <kimeodml@gmail.com>
2023-08-06 22:50:53 +0900

    feat: 'shops/maps' api 연결

--- src/api/shop/index.ts
+++ src/api/shop/index.ts
@@ -1,8 +1,16 @@
-import { FetchShopsResponse, FetchTrendingsResponse, ShopsParams } from './entity';
-import shophApi from './shopApiClient';
+import {
+  Coords,
+  FetchShopsResponse, FetchTrendingsResponse, FilterShopsParams, FilterShopsResponse, ShopsParams,
+} from './entity';
+import shopApi from './shopApiClient';
 
-export const fetchTrendings = () => shophApi.get<FetchTrendingsResponse>('/trending');
+export const fetchTrendings = () => shopApi.get<FetchTrendingsResponse>('/trending');
 
-export const fetchShop = (shopId: string) => shophApi.get(`/shop?place_id=${shopId}`);
+export const fetchShop = (shopId: string) => shopApi.get(`/shop?place_id=${shopId}`);
 
-export const fetchShops = (params: ShopsParams) => shophApi.post<FetchShopsResponse>(`/shops?keyword=${params.keyword}&x=${params.location?.latitude}&y=${params.location?.longitude}`);
+export const fetchShops = (params: ShopsParams) => shopApi.post<FetchShopsResponse>(`/shops?keyword=${params.keyword}&x=${params.location?.latitude}&y=${params.location?.longitude}`);
+
+export const getfilterShops = (params: FilterShopsParams, location: Coords) => shopApi.post<FilterShopsResponse>(`/shops/maps?options_friend=${params.options_friend}&options_nearby=${params.options_nearby}&options_scrap=${params.options_friend}`, {
+  lat: location.latitude,
+  lng: location.longitude,
+});
