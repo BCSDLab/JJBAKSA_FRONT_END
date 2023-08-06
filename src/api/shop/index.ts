5d93665edee34d4556de4e5b25970c7415caaa29
Daeeui Kim <kimeodml@gmail.com>
2023-08-07 00:52:17 +0900

    fix: fetchShops, getfilterShops api 수정

--- src/api/shop/index.ts
+++ src/api/shop/index.ts
@@ -1,6 +1,6 @@
 import {
-  Coords,
-  FetchShopsResponse, FetchTrendingsResponse, FilterShopsParams, FilterShopsResponse, ShopsParams,
+  Coords, FetchShopsResponse, FetchTrendingsResponse, FilterShopsParams,
+  FilterShopsListResponse, ShopsParams,
 } from './entity';
 import shopApi from './shopApiClient';
 
@@ -8,9 +8,12 @@ export const fetchTrendings = () => shopApi.get<FetchTrendingsResponse>('/trendi
 
 export const fetchShop = (shopId: string) => shopApi.get(`/shop?place_id=${shopId}`);
 
-export const fetchShops = (params: ShopsParams) => shopApi.post<FetchShopsResponse>(`/shops?keyword=${params.keyword}&x=${params.location?.latitude}&y=${params.location?.longitude}`);
+export const fetchShops = (params: ShopsParams) => shopApi.post<FetchShopsResponse>(`/shops?keyword=${params.keyword}`, {
+  lat: params.location?.lat,
+  lng: params.location?.lng,
+});
 
-export const getfilterShops = (params: FilterShopsParams, location: Coords) => shopApi.post<FilterShopsResponse>(`/shops/maps?options_friend=${params.options_friend}&options_nearby=${params.options_nearby}&options_scrap=${params.options_friend}`, {
-  lat: location.latitude,
-  lng: location.longitude,
+export const getfilterShops = (params: FilterShopsParams, location: Coords) => shopApi.post<FilterShopsListResponse>(`/shops/maps?options_friend=${params.options_friend}&options_nearby=${params.options_nearby}&options_scrap=${params.options_friend}`, {
+  lat: location.lat,
+  lng: location.lng,
 });
