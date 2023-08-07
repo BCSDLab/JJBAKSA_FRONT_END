5324bf09c8630534ed6451f725b2f502e2fc8c4e
Daeeui Kim <kimeodml@gmail.com>
2023-08-07 21:49:22 +0900

    fix: shop/maps 응답 데이터에 맞게 더미 데이터 값 변경

--- src/pages/Home/static/marker.ts
+++ src/pages/Home/static/marker.ts
@@ -1,51 +1,25 @@
 const MARKER = [
   {
-    placeName: '박순자 아우내순대',
-    latitude: 36.761724,
-    longitude: 127.299092,
-    index: 1,
+    geometry: {
+      location: {
+        lat: 37.5108295,
+        lng: 127.0292881,
+      },
+    },
+    name: '고기',
+    photo: ['string', 'string'],
+    placeId: 'string',
   },
   {
-    placeName: '집',
-    latitude: 36.343087,
-    longitude: 127.448879,
-    index: 2,
-  },
-  {
-    placeName: '3',
-    latitude: 36.722341,
-    longitude: 127.269665,
-    index: 3,
-  },
-  {
-    placeName: '4',
-    latitude: 37.762341,
-    longitude: 125.189675,
-    index: 4,
-  },
-  {
-    placeName: '5',
-    latitude: 36.862341,
-    longitude: 127.254631,
-    index: 5,
-  },
-  {
-    placeName: '6',
-    latitude: 36.962341,
-    longitude: 127.218645,
-    index: 6,
-  },
-  {
-    placeName: '7',
-    latitude: 36.562341,
-    longitude: 127.299565,
-    index: 7,
-  },
-  {
-    placeName: '8',
-    latitude: 36.552341,
-    longitude: 126.259775,
-    index: 8,
+    geometry: {
+      location: {
+        lat: 37,
+        lng: 127,
+      },
+    },
+    name: '고기2',
+    photo: ['string'],
+    placeId: 'string',
   },
 ];
 
