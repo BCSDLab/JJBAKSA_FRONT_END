447f40f7d958ff6e685b8f56c1a6722ca8779a6c
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2023-08-12 18:15:34 +0900

    style: 웹뷰 UI 모바일 버전 UI로 통일

--- src/pages/MyPage/components/Information/index.tsx
+++ src/pages/MyPage/components/Information/index.tsx
@@ -1,7 +1,6 @@
 import defaultImage from 'assets/images/follow/default-image.png';
 // import fixPencil from 'assets/images/mypage/pencil.svg';
 import option from 'assets/svg/mypage/option.svg';
-import useMediaQuery from 'utils/hooks/useMediaQuery';
 import useMyProfile from 'pages/MyPage/hooks/useMyProfile';
 import styles from './Information.module.scss';
 
@@ -10,7 +9,6 @@ interface InformationProps {
 }
 
 export default function Information({ openModal }:InformationProps) {
-  const { isMobile } = useMediaQuery();
   const { profile } = useMyProfile();
 
   return (
@@ -24,28 +22,12 @@ export default function Information({ openModal }:InformationProps) {
           <span className={styles.user__nickname}>
             {profile?.nickname}
           </span>
-          {isMobile && (
-          <>
-            <div className={styles.dot} />
-            <span className={styles['totals__follower--number']}>팔로워 120</span>
-          </>
-          )}
+          <div className={styles.dot} />
+          <span className={styles['totals__follower--number']}>팔로워 120</span>
           <span className={styles.user__account}>{profile?.account}</span>
         </div>
       </div>
-      {!isMobile && (
-      <div className={styles.totals}>
-        <div className={styles.totals__post}>
-          <span className={styles['totals__post--number']}>{profile?.userCountResponse.reviewCount}</span>
-          <span className={styles['totals__post--label']}>게시물</span>
-        </div>
-        <div className={styles.totals__follower}>
-          <span className={styles['totals__follower--number']}>22</span>
-          <span className={styles['totals__follower--label']}>팔로워</span>
-        </div>
-      </div>
-      )}
-      {isMobile && <img src={option} alt="option" className={styles.option} />}
+      <img src={option} alt="option" className={styles.option} />
     </div>
   );
 }
--- src/pages/MyPage/components/MyPost/index.tsx
+++ src/pages/MyPage/components/MyPost/index.tsx
@@ -1,38 +1,14 @@
-import { useState } from 'react';
-import checkerboard from 'assets/svg/mypage/checkerboard.svg';
-import filledCheckerboard from 'assets/svg/mypage/checkerboard-filled.svg';
-import filledHamberger from 'assets/svg/mypage/hamberger-filled.svg';
-import hamberger from 'assets/svg/mypage/hamberger.svg';
-import useMediaQuery from 'utils/hooks/useMediaQuery';
 import notExist from 'assets/svg/mypage/not-exist.svg';
 import useReviwedShops from 'pages/MyPage/hooks/useReviewedShops';
 import styles from './MyPost.module.scss';
-import CheckerBoard from '../Checkerboard';
-import DUMMY from './dummy';
-import HambergerBoard from '../HambergerBoard';
 import MobileBoard from '../MobileBoard';
 
 export default function MyPost() {
-  const [viewType, setType] = useState('CHECKERBOARD');
   const { isLoading, isError, shops } = useReviwedShops();
-  console.log(isLoading, isError, shops);
-  const { isMobile } = useMediaQuery();
   return (
     <div>
       {!isLoading && !isError && shops?.length !== 0 ? (
-        <>
-          <div className={styles['view-options']}>
-            <button type="button" onClick={() => setType('CHECKERBOARD')} className={styles['view-options__option']}>
-              <img src={viewType === 'CHECKERBOARD' ? filledCheckerboard : checkerboard} alt="checkerboard" />
-            </button>
-            <button type="button" onClick={() => setType('HAMBERGER')} className={styles['view-options__option']}>
-              <img src={viewType === 'HAMBERGER' ? filledHamberger : hamberger} alt="checkerboard" />
-            </button>
-          </div>
-          {!isMobile && viewType === 'CHECKERBOARD' && <CheckerBoard posts={DUMMY} />}
-          {!isMobile && viewType === 'HAMBERGER' && <HambergerBoard posts={DUMMY} />}
-          {isMobile && <MobileBoard posts={shops} />}
-        </>
+        <MobileBoard posts={shops} />
       ) : (
         (
           <div className={styles['not-exist']}>
