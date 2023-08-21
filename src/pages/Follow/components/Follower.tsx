a4a6b5ca13217bdf97551c27c89f523889c712b4
채승윤 <thslr57@naver.com>
2023-08-21 21:38:09 +0900

    feat: UI 변경

--- src/pages/Follow/components/Follower.tsx
+++ src/pages/Follow/components/Follower.tsx
@@ -1,7 +1,11 @@
 import defaultImage from 'assets/images/follow/default-image.png';
 import cn from 'utils/ts/classNames';
-import { acceptFollow, requestFollow } from 'api/follow';
+import {
+  acceptFollow, cancleFollow, deleteFollow, requestFollow,
+} from 'api/follow';
 import { useMutation, useQueryClient } from 'react-query';
+import { ReactComponent as Unfollow } from 'assets/svg/follow/user-remove.svg';
+import { ReactComponent as Follow } from 'assets/svg/follow/user-add.svg';
 import style from './Follower.module.scss';
 
 // 팔로우 요청 후 유저 목록을 다시 받아와 요청중 상태로 변경
@@ -9,6 +13,7 @@ const useRequestAndUpdate = () => {
   const queryClient = useQueryClient();
   const { mutate: request } = useMutation('request', (account: string) => requestFollow({ userAccount: account }), {
     onSuccess: () => {
+      queryClient.invalidateQueries('sended');
       queryClient.invalidateQueries('search');
     },
   });
@@ -21,9 +26,31 @@ const useAcceptFollow = () => {
   const { mutate: accept } = useMutation('accept', (id: number) => acceptFollow({ id }), {
     onSuccess: () => {
       queryClient.invalidateQueries('received');
+      queryClient.invalidateQueries('follower');
     },
   });
-  return { accept };
+  return accept;
+};
+const useDeleteFollow = () => {
+  const queryClient = useQueryClient();
+  const { mutate: del } = useMutation('delete', (account: string) => deleteFollow({ userAccount: account }), {
+    onSuccess: () => {
+      queryClient.invalidateQueries('follower');
+    },
+  });
+
+  return del;
+};
+
+const useCancleFollow = () => {
+  const queryClient = useQueryClient();
+  const { mutate: cancle } = useMutation('cancel', (id: number) => cancleFollow({ id }), {
+    onSuccess: () => {
+      queryClient.invalidateQueries('sended');
+    },
+  });
+
+  return cancle;
 };
 
 interface Props {
@@ -31,33 +58,44 @@ interface Props {
   nickname: string,
   followedType: string,
   id: number,
+  requestId?: number,
 }
 
 export default function Follower({
-  nickname, account, followedType, id,
+  nickname, account, followedType, id, requestId,
 }: Props) {
   const request = useRequestAndUpdate();
-  const { accept } = useAcceptFollow();
-
+  const accept = useAcceptFollow();
+  const del = useDeleteFollow();
+  const cancle = useCancleFollow();
   return (
-    <div className={style.follower}>
+    <div className={style.follower} id={`${id}`}>
       <img className={style.follower__image} src={defaultImage} alt="default" />
       <div className={style.follower__content}>
         <p className={cn({ [style['follower__content--font']]: true })}>{nickname}</p>
-        <p>{account}</p>
+        <p>
+          @
+          {account}
+        </p>
       </div>
       <button
         className={cn({
-          [style.follower__button]: followedType === 'NONE' || followedType === 'RECEIVED',
-          [style['follower__button--unfollow']]: followedType === 'REQUESTED' || followedType === 'FOLLOWED',
+          [style.follower__button]: followedType === 'NONE' || followedType === 'FOLLOWED',
+          [style['follower__button--cancel']]: followedType === 'REQUEST_SENT',
+          [style['follower__button--accept']]: followedType === 'REQUEST_RECEIVE',
         })}
         type="button"
-        onClick={() => (followedType === 'NONE' && request(account)) || (followedType === 'RECEIVED' && accept(id))}
+        onClick={
+          () => (followedType === 'NONE' && request(account))
+            || (followedType === 'REQUEST_RECEIVE' && requestId && accept(requestId))
+            || (followedType === 'FOLLOWED' && del(account))
+            || (followedType === 'REQUEST_SENT' && requestId && cancle(requestId))
+        }
       >
-        {followedType === 'NONE' && '팔로우'}
-        {followedType === 'REQUESTED' && '요청중'}
-        {followedType === 'FOLLOWED' && '언팔로우'}
-        {followedType === 'RECEIVED' && '팔로우'}
+        {followedType === 'NONE' && <Follow />}
+        {followedType === 'REQUEST_SENT' && '신청 취소'}
+        {followedType === 'FOLLOWED' && <Unfollow />}
+        {followedType === 'REQUEST_RECEIVE' && '친구 수락'}
       </button>
     </div>
   );
