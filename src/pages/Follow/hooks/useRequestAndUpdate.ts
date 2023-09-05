e7a7ad1eac242f3edec275c22ae526ef56106f4d
채승윤 <thslr57@naver.com>
2023-09-05 11:22:51 +0900

    fix: 잘못된 계정에 친구 신청한 경우 에러 처리

--- src/pages/Follow/hooks/useRequestAndUpdate.ts
+++ src/pages/Follow/hooks/useRequestAndUpdate.ts
@@ -1,5 +1,6 @@
 import { requestFollow } from 'api/follow';
 import { useMutation, useQueryClient } from 'react-query';
+import makeToast from 'utils/ts/makeToast';
 
 // 팔로우 요청 후 유저 목록을 다시 받아와 요청중 상태로 변경
 const useRequestAndUpdate = () => {
@@ -8,6 +9,9 @@ const useRequestAndUpdate = () => {
     onSuccess: () => {
       queryClient.invalidateQueries('sended');
       queryClient.invalidateQueries('search');
+    },
+    onError: () => {
+      makeToast('error', '잘못된 친구 정보입니다.');
     },
   });
   return request;
