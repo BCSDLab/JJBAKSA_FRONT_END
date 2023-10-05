34a7528a7dad329ec1c5124f45515d1b24c9e6ba
Chaeseungyun <thslr57@naver.com>
2023-10-05 17:42:37 +0900

    feat: 모바일 화면 친구 취소 모달 창 추가

--- src/pages/Follow/hooks/useDeleteFollow.ts
+++ src/pages/Follow/hooks/useDeleteFollow.ts
@@ -1,15 +1,27 @@
 import { deleteFollow } from 'api/follow';
 import { useMutation, useQueryClient } from 'react-query';
+import useBooleanState from 'utils/hooks/useBooleanState';
+import useMediaQuery from 'utils/hooks/useMediaQuery';
 
 const useDeleteFollow = () => {
   const queryClient = useQueryClient();
+  const { isMobile } = useMediaQuery();
+  const [value,,,toggle] = useBooleanState(false);
   const { mutate: del, data } = useMutation('delete', (account: string) => deleteFollow({ userAccount: account }), {
     onSuccess: () => {
       queryClient.invalidateQueries('follower');
     },
   });
 
-  return { del, data };
+  const mobileUnfollow = () => {
+    if (isMobile) {
+      toggle();
+    }
+  };
+
+  return {
+    del, data, value, mobileUnfollow, isMobile, toggle,
+  };
 };
 
 export default useDeleteFollow;
