83b20df47c1c5303c659a6c67e228ea3fa9ec813
Daeeui Kim <kimeodml@gmail.com>
2023-10-09 22:01:06 +0900

    fix: 모바일 필터 버튼 수정

--- src/pages/Home/components/Map/components/MobileOptions/index.tsx
+++ src/pages/Home/components/Map/components/MobileOptions/index.tsx
@@ -1,66 +1,74 @@
 import { ReactComponent as LensIcon } from 'assets/svg/home/lens.svg';
-import { ReactComponent as GroupIcon } from 'assets/svg/home/group.svg';
+import { ReactComponent as StoreFrontIcon } from 'assets/svg/home/storefront.svg';
 import { ReactComponent as BookMarkIcon } from 'assets/svg/home/bookmark.svg';
-import { ReactComponent as VerticalDot } from 'assets/svg/home/verticalDot.svg';
+import { ReactComponent as GroupIcon } from 'assets/svg/home/group.svg';
+import { ReactComponent as VerticalDot } from 'assets/svg/home/verticaldot.svg';
 import { Link } from 'react-router-dom';
-import cn from 'utils/ts/classNames';
-import { useState } from 'react';
 import useBooleanState from 'utils/hooks/useBooleanState';
+import { useFilterFriend, useFilterNearby, useFilterScrap } from 'store/filter';
+import cn from 'utils/ts/classNames';
 import styles from './MobileOptions.module.scss';
 
 export default function MobileOptions(): JSX.Element {
-  const [filter, , ,toggle] = useBooleanState(false);
-  const [selected, setSelected] = useState('');
-  const handleClick = (type: string) => {
-    setSelected(type);
-    // 각각의 함수 추가 필요
-  };
+  const [visible, , , toggle] = useBooleanState(true);
+  const { filterFriendState, setFilterFriend } = useFilterFriend();
+  const { filterScrapState, setFilterScrap } = useFilterScrap();
+  const { filterNearbyState, setFilterNearby } = useFilterNearby();
+
   return (
-    <div className={styles.options}>
-      <div className={styles.nav}>
-        <div>
-          <Link to="/search" className={styles.nav__search}>
-            검색어를 입력해주세요.
-            <LensIcon />
-          </Link>
-        </div>
-        <button
-          type="button"
-          className={cn({
-            [styles.nav__filter]: true,
-            [styles['nav__filter--clicked']]: selected !== '' && !filter,
-          })}
-          onClick={toggle}
-        >
-          <VerticalDot />
-        </button>
-        {filter && (
-        <div className={styles.nav__list}>
+    <div className={styles.nav}>
+      <Link to="/search" className={styles.nav__search}>
+        검색어를 입력해주세요.
+        <LensIcon />
+      </Link>
+      <button
+        type="button"
+        className={cn({
+          [styles.nav__icon]: true,
+          [styles['nav__icon--changed']]: filterNearbyState === 1 || filterScrapState === 1 || filterFriendState === 1,
+        })}
+        onClick={toggle}
+        aria-label="펼치기"
+      >
+        <VerticalDot />
+      </button>
+      {visible && (
+        <div className={styles.filter}>
           <button
             type="button"
             className={cn({
-              [styles.nav__text]: true,
-              [styles['nav__text--clicked']]: selected === 'friend',
+              [styles.filter__text]: true,
+              [styles['filter__text--clicked']]: filterNearbyState === 1,
             })}
-            onClick={() => handleClick('friend')}
+            onClick={() => { setFilterNearby(filterNearbyState === 0 ? 1 : 0); }}
           >
-            <GroupIcon />
-            친구 음식점
+            <StoreFrontIcon />
+            가까운 음식점
           </button>
           <button
             type="button"
             className={cn({
-              [styles.nav__text]: true,
-              [styles['nav__text--clicked']]: selected === 'bookmark',
+              [styles.filter__text]: true,
+              [styles['filter__text--clicked']]: filterScrapState === 1,
             })}
-            onClick={() => handleClick('bookmark')}
+            onClick={() => { setFilterScrap(filterScrapState === 0 ? 1 : 0); }}
           >
             <BookMarkIcon />
             북마크 음식점
           </button>
+          <button
+            type="button"
+            className={cn({
+              [styles.filter__text]: true,
+              [styles['filter__text--clicked']]: filterFriendState === 1,
+            })}
+            onClick={() => { setFilterFriend(filterFriendState === 0 ? 1 : 0); }}
+          >
+            <GroupIcon />
+            친구 음식점
+          </button>
         </div>
-        )}
-      </div>
+      )}
     </div>
   );
 }
