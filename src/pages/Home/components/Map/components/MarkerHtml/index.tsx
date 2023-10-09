2651e4c10039b08bd27256604690b76386157656
Daeeui Kim <kimeodml@gmail.com>
2023-10-09 22:25:41 +0900

    fix: 마커에 음식점 이미지 표시

--- src/pages/Home/components/Map/components/MarkerHtml/index.tsx
+++ src/pages/Home/components/Map/components/MarkerHtml/index.tsx
@@ -1,15 +1,15 @@
-import Default from 'assets/images/search/default-image.png';
 import MarkerLogo from 'assets/images/home/location-marker.png';
 import Selected from 'assets/images/home/selected-marker.png';
+import defaultImage from 'assets/images/home/favicon.png';
 import styles from './MarkerHtml.module.scss';
 
-export function MarkerHtml(photo:string | null, name:string) {
+export function MarkerHtml(photo:string, name:string) {
   return `
   <div class=${styles.container}>
     <img src=${MarkerLogo}  class=${styles.marker} alt="marker" />
     <div class=${styles.bubble}>
       <div class=${styles.bubble__photo}>
-        <img src=${Default} alt="사진"/>
+        <img src=${photo || `${defaultImage}`} alt="사진"/>
       </div>
       <div class=${styles.bubble__name}>
         ${name}
@@ -19,13 +19,13 @@ export function MarkerHtml(photo:string | null, name:string) {
   `;
 }
 
-export function ClickedMarkerHtml(photo:string | null, name:string) {
+export function ClickedMarkerHtml(photo:string, name:string) {
   return `
   <div class=${styles.continer}>
     <img src=${Selected} class=${styles['marker--clicked']}>
     <div class=${styles.bubble}>
       <div class=${styles['bubble__photo--clicked']}>
-        <img src=${Default} alt="사진" />
+        <img src=${photo || `${defaultImage}`} alt="사진" />
       </div>
       <div class=${styles['bubble__name--clicked']}>
         ${name}
