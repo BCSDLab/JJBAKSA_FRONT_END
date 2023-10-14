c5bb4fd566c1e606455e4c118098c668d59e3108
Chaeseungyun <thslr57@naver.com>
2023-10-15 02:28:02 +0900

    refactor: pc, 모바일 평점 UI 변경

--- src/pages/Follow/components/ListReview.tsx
+++ src/pages/Follow/components/ListReview.tsx
@@ -1,4 +1,6 @@
 import { ReactComponent as Rating } from 'assets/svg/follow/rate.svg';
+import { ReactComponent as EmptyStar } from 'assets/svg/follow/empty-star.svg';
+import useMediaQuery from 'utils/hooks/useMediaQuery';
 import style from './ListReview.module.scss';
 
 interface Props {
@@ -8,15 +10,26 @@ interface Props {
 }
 
 export default function ListReview({ createdAt, content, rate }: Props) {
+  const { isMobile } = useMediaQuery();
+  const rateArray = new Array<number>(rate).fill(1);
+  const restArray = new Array<number>(5 - rate).fill(1);
   return (
     <div className={style.container}>
       {content}
       <div>
         {createdAt}
         {' | '}
-        <Rating />
-        {rate}
-        .0
+        {isMobile ? (
+          <span>
+            {rate}
+            .0
+          </span>
+        ) : (
+          <span>
+            {rateArray.map(() => <Rating />)}
+            {restArray.map(() => <EmptyStar />)}
+          </span>
+        )}
       </div>
     </div>
   );
