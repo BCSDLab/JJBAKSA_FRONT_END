88cc31f354d83b6d2cc57a5a0e71a9a8b6ec364f
Chaeseungyun <thslr57@naver.com>
2023-10-14 14:19:58 +0900

    fix: 다른 유저의 리뷰 개수 api의 호출이 정상적으로 이루어지도록 수정

--- src/pages/Follow/hooks/useGetFollowerReviewCount.ts
+++ src/pages/Follow/hooks/useGetFollowerReviewCount.ts
@@ -2,7 +2,7 @@ import { getFollowerReviewCount } from 'api/follow';
 import { useQuery } from 'react-query';
 
 const useGetFollowerReviewCount = (followId: number) => {
-  const { data } = useQuery('reviewCount', () => getFollowerReviewCount({ followId }));
+  const { data } = useQuery(['reviewCount', followId], () => getFollowerReviewCount({ followId }));
   return data;
 };
 
