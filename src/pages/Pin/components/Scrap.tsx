94bab7716d15208ff6023535644e6125eed55acd
KimDoToRi <60084235+KimKyungYun@users.noreply.github.com>
2023-10-17 16:05:24 +0900

    fix: 스크랩동작에 따라 스크랩목록을 다시 받아오도록 수정

--- src/pages/Pin/components/Scrap.tsx
+++ src/pages/Pin/components/Scrap.tsx
@@ -1,6 +1,6 @@
 import { ReactComponent as BookMark } from 'assets/svg/pin/bookmark.svg';
 import { ReactComponent as BookMarkActivated } from 'assets/svg/pin/bookmark_activated.svg';
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 import { getScrap, deleteScrap, postScrap } from 'api/scrap';
 import { useEffect, useState } from 'react';
 import styles from '../Pin.module.scss';
@@ -9,8 +9,10 @@ interface Props {
   placeId:string;
 }
 export default function Scrap({ placeId }:Props) {
+  const queryClient = useQueryClient();
   const scrapList = useQuery('scrapList', () => getScrap());
   const [scrapId, setScrapId] = useState<number>(-1);
+  console.log(scrapId);
 
   useEffect(() => {
     if (scrapList.status === 'success') {
@@ -18,19 +20,24 @@ export default function Scrap({ placeId }:Props) {
     }
   }, [placeId, scrapList.data, scrapList.status]);
 
+  const handleScrap = async () => {
+    if (scrapId === -1) {
+      postScrap({ directoryId: 0, placeId })
+        .then((response) => {
+          setScrapId(response.id);
+          queryClient.removeQueries('scrapList');
+        });
+    } else {
+      await deleteScrap(scrapId);
+      queryClient.removeQueries('scrapList');
+      setScrapId(-1);
+    }
+  };
   return (
     <button
       className={styles.shop__bookmark}
       type="button"
-      onClick={() => {
-        if (scrapId === -1) {
-          postScrap({ directoryId: 0, placeId })
-            .then((response) => setScrapId(response.id));
-        } else {
-          deleteScrap(scrapId);
-          setScrapId(-1);
-        }
-      }}
+      onClick={handleScrap}
     >
       {scrapId === -1
         ? <BookMark />
