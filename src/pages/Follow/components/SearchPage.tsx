0f191a7add8735127e4b3b80aa60748653d13466
Somin <ksomin03@koreatech.ac.kr>
2023-10-22 23:09:56 +0900

    refactor: 타입단언 삭제

--- src/pages/Follow/components/SearchPage.tsx
+++ src/pages/Follow/components/SearchPage.tsx
@@ -1,17 +1,22 @@
 import { useAuth } from 'store/auth';
-import { EmailUser } from 'api/user/entity';
 import { FollowerInfo, SearchPageInfo } from '../static/entity';
 import FollowList from './FollowList';
 
 export default function SearchPage({ data }: SearchPageInfo) {
-  const auth = useAuth() as EmailUser;
+  const auth = useAuth();
   const myFriends: FollowerInfo[] = data.filter((follower) => follower.followedType === 'FOLLOWED');
-  const newFriends: FollowerInfo[] = data.filter((follower) => follower.followedType === 'NONE').filter((follower) => follower.account !== auth?.account);
+  let newFriends: FollowerInfo[];
 
+  if (auth && 'account' in auth) {
+    newFriends = data.filter((follower) => follower.followedType === 'NONE').filter((follower) => follower.account !== auth?.account);
+    return (
+      <div>
+        <FollowList title="나의 친구" data={myFriends} />
+        <FollowList title="새 친구" data={newFriends} />
+      </div>
+    );
+  }
   return (
-    <div>
-      <FollowList title="나의 친구" data={myFriends} />
-      <FollowList title="새 친구" data={newFriends} />
-    </div>
+    <div>오류입니다.</div>
   );
 }
