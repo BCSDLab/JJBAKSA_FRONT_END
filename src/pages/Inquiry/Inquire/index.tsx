d658159ddffadb7c0f74757da7f834b1a072de41
정해성 <gotjd0230@naver.com>
2023-11-04 01:54:05 +0900

    refactor: Inquire 페이지 수정
    - Explain 컴포넌트 분리
    - button --> Link

--- src/pages/Inquiry/Inquire/index.tsx
+++ src/pages/Inquiry/Inquire/index.tsx
@@ -1,50 +1,20 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { ReactComponent as WriteInquiryIcon } from 'assets/svg/inquiry/write-inquiry.svg';
+import { Link } from 'react-router-dom';
 import styles from './Inquire.module.scss';
-
-function Explain(): JSX.Element {
-  return (
-    <div className={styles.explain}>
-      <div className={styles.explain__svg}>
-        <WriteInquiryIcon />
-      </div>
-
-      <div className={styles.explain__title}>
-        쩝쩝박사 이용 중에 생긴 불편한 점이나 문의사항을 등록해주세요.
-      </div>
-
-      <div className={styles['explain__small-title']}>
-        확인 후 순차적으로 답변 드리겠습니다.
-      </div>
-    </div>
-  );
-}
+import Explain from './components/Explain';
 
 export default function Inquiry(): JSX.Element {
-  const navigate = useNavigate();
-
   return (
     <div className={styles.container}>
       <div className={styles.box}>
         <div className={styles.menu}>
-          <button
-            className={styles.menu__title}
-            onClick={() => {
-              navigate('/inquiry/all');
-            }}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter' || e.key === ' ') {
-                navigate('/inquiry/all');
-              }
-            }}
-            type="button"
-            tabIndex={0}
-          >
-            문의하기
-          </button>
+          <Link to="/inquiry/all" className={styles.menu__link}>
+            <div className={styles['menu__title-text']}>
+              문의하기
+            </div>
+          </Link>
 
-          <Explain />
+          <Explain className={styles.menu__explain} />
         </div>
 
         <div className={styles.form}>
