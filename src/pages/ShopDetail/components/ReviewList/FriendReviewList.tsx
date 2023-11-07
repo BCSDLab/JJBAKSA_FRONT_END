2e3a48706b6a47fdb2c29a91ff4e1ce89986b46c
d0dam <smallkdb@gmail.com>
2023-11-07 17:01:44 +0900

    feat: Review 관련 컴포넌트 구현

---
+++ src/pages/ShopDetail/components/ReviewList/FriendReviewList.tsx
@@ -0,0 +1,56 @@
+import { fetchFollowerReview } from 'api/review';
+import { useQuery } from 'react-query';
+import styles from './ReviewList.module.scss';
+import SectionHeader from '../SectionHeader';
+import NotFoundDescription from '../NotFoundDescription';
+// import mockReviews from './mock';
+
+interface Props {
+  placeId: string;
+}
+
+function FriendReviewList({ placeId }: Props) {
+  const { data } = useQuery('followerReviews', () => fetchFollowerReview(placeId));
+
+  if (data) {
+    const { content, totalElements, size } = data.data;
+
+    return (
+      <section className={styles['review-list']}>
+        <SectionHeader
+          title="친구의 리뷰"
+          description={`총 ${totalElements}개의 리뷰`}
+          button={totalElements > size ? { name: '전체보기', handler: () => {} } : undefined}
+        />
+
+        {content.length ? (
+          <ul className={styles['review-list__main']}>
+            {content.slice(0, size).map(({ id, content: reviewContent, userReviewResponse }) => (
+              <li key={id}>
+                <img
+                  alt={`${userReviewResponse.nickname}의 프로필`}
+                  src={userReviewResponse?.profileImage?.url}
+                />
+                <div className={styles['review-list__main__follower-content']}>
+                  <h3>{userReviewResponse.nickname}</h3>
+                  <div>{reviewContent}</div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <NotFoundDescription />
+        )}
+      </section>
+    );
+  }
+
+  return (
+    <section className={styles['review-list']}>
+      <SectionHeader title="친구의 리뷰" description="리뷰를 불러올 수 없습니다." />
+      <NotFoundDescription />
+    </section>
+  );
+}
+
+export default FriendReviewList;
---
+++ src/pages/ShopDetail/components/ReviewList/MyReviewList.tsx
@@ -0,0 +1,58 @@
+import { fetchMyReview } from 'api/review';
+import { useQuery } from 'react-query';
+import formatISODate from 'utils/ts/formatISODate';
+import styles from './ReviewList.module.scss';
+import SectionHeader from '../SectionHeader';
+import NotFoundDescription from '../NotFoundDescription';
+// import mockReviews from './mock';
+
+interface Props {
+  placeId: string;
+}
+
+function MyReviewList({ placeId }: Props) {
+  const { data } = useQuery('myReviews', () => fetchMyReview(placeId));
+
+  if (data) {
+    const { content } = data.data;
+
+    return (
+      <section className={styles['review-list']}>
+        <SectionHeader
+          title="나의 리뷰"
+          description={
+            content[0]
+              ? `마지막 방문 ${formatISODate(content[0].createdAt)}`
+              : '방문 기록이 없어요.'
+          }
+        />
+        {content.length ? (
+          <ul className={styles['review-list__main']}>
+            {content.map(({ id, content: reviewContent, userReviewResponse }) => (
+              <li key={id}>
+                <img
+                  alt={`${userReviewResponse.nickname}의 프로필`}
+                  src={userReviewResponse?.profileImage?.url}
+                />
+                <div className={styles['review-list__main__my-content']}>
+                  <div>{reviewContent}</div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <NotFoundDescription />
+        )}
+      </section>
+    );
+  }
+
+  return (
+    <section className={styles['review-list']}>
+      <SectionHeader title="나의 리뷰" description="리뷰를 불러올 수 없습니다." />
+      <NotFoundDescription />
+    </section>
+  );
+}
+
+export default MyReviewList;
