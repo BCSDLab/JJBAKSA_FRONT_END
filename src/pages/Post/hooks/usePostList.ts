f4b572b70c9e217126ca90522db3d630f7282453
최원빈 <wonbeen@toss.im>
2023-11-10 18:17:20 +0900

    feat: Suspense 예제 추가

--- src/pages/Post/hooks/usePostList.ts
+++ src/pages/Post/hooks/usePostList.ts
@@ -1,10 +1,10 @@
 import getPost from 'api/Post';
-import { useQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 
 const usePostList = (page: number) => {
   const {
     isLoading, isError, data,
-  } = useQuery({ queryKey: ['Post', page], queryFn: () => getPost(page) });
+  } = useSuspenseQuery({ queryKey: ['Post', page], queryFn: () => getPost(page) });
 
   return {
     isLoading, isError, data,
