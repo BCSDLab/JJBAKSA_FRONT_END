1141eab09c67ef5d62f21da7a3379c54b5333d63
Daeeui Kim <kimeodml@gmail.com>
2023-11-16 20:39:16 +0900

    fix: 타입 및 구조 변경

--- src/mocks/api/shopsResult.ts
+++ src/mocks/api/shopsResult.ts
@@ -3,26 +3,25 @@ import { API_PATH } from 'config/constants';
 import shopsResultData from './data/shopsResultData';
 
 interface RequestBody {
-  lat: number | undefined;
-  lng: number | undefined;
+  lat: number;
+  lng: number;
 }
 
 const handlers = [
   http.post(`${API_PATH}/shops`, async ({ request }) => {
     const body = await request.json() as RequestBody;
 
-    body.lat = 37.71;
-    body.lng = 127.97;
-
     const url = new URL(request.url.toString());
     const keyword = url.searchParams.get('keyword');
 
     if (typeof body.lat !== 'number' || typeof body.lng !== 'number') {
-      return new HttpResponse(null, { status: 401 });
+      return new HttpResponse(null, { status: 500 });
     }
 
-    const filteredShops = shopsResultData.filter((shop) => shop.name.includes(keyword ?? ''));
+    const filteredShops = shopsResultData.filter((shop) => keyword && shop.name.includes(keyword));
+
     return HttpResponse.json({
+      pageToken: null,
       shopQueryResponseList: filteredShops,
     });
   }),
