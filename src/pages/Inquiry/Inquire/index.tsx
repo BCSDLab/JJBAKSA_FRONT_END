632052a758fa3723fced021e67482b3305faa836
정해성 <gotjd0230@naver.com>
2023-11-30 02:46:53 +0900

    feat: content UI 구성

--- src/pages/Inquiry/Inquire/index.tsx
+++ src/pages/Inquiry/Inquire/index.tsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import useBooleanState from 'utils/hooks/useBooleanState';
 import { Link } from 'react-router-dom';
+import { ReactComponent as UploadIcon } from 'assets/svg/inquiry/image-upload.svg';
 import ToggleButton from 'components/common/ToggleButton';
 import Explain from './components/Explain';
 import styles from './Inquire.module.scss';
 
 export default function Inquiry(): JSX.Element {
+  const [content, setContent] = useState('');
+  const maxLength = 500;
   const [a, , , toggle] = useBooleanState(false);
+
+  const handleContentChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(event.target.value);
+  };
 
   return (
     <div className={styles.container}>
@@ -22,24 +30,40 @@ export default function Inquiry(): JSX.Element {
 
         <div className={styles['content-box']}>
           <form className={styles.form}>
-            <label htmlFor="inquiryTitle">
-              <div className={styles.form__label}>
-                제목
+            <div className={styles.form__title}>
+              <div className={styles['form__label-box']}>
+                <div className={styles.form__label}>
+                  제목
+                </div>
+                <div className={styles['form__essential-circle-box']}>
+                  <span className={styles['form__essential-circle']} />
+                </div>
               </div>
               <input
-                className={styles.form__title}
+                className={styles['form__title-input']}
                 name="inquiryTitle"
                 type="text"
                 required
                 id="inquiryTitle"
+                maxLength={50}
                 placeholder="제목을 작성해주세요."
               />
-            </label>
+            </div>
 
-            <label htmlFor="inquiryContent">
-              <div className={styles.form__label}>
-                문의 내용
+            <div className={styles['form__content-information']}>
+              <div className={styles['form__label-box']}>
+                <div className={styles.form__label}>
+                  문의 내용
+                </div>
+                <div className={styles['form__essential-circle-box']}>
+                  <span className={styles['form__essential-circle']} />
+                </div>
               </div>
+              <div className={styles['form__character-count']}>
+                {`${content.length}/${maxLength}`}
+              </div>
+            </div>
+            <div className={styles['form__content-box']}>
               <textarea
                 className={styles.form__content}
                 name="inquiryContent"
@@ -48,10 +72,22 @@ export default function Inquiry(): JSX.Element {
                 rows={10}
                 maxLength={500}
                 placeholder="문의 내용을 작성해주세요."
+                value={content}
+                onChange={handleContentChange}
               />
-            </label>
+              <div className={styles.form__upload}>
+                <UploadIcon />
+                <input
+                  className={styles['form__image-input']}
+                  type="file"
+                  accept="image/*"
+                  aria-label="이미지 업로드"
+                />
+                <span className={styles['form__upload-explain']}>최대 3장 첨부 가능</span>
+              </div>
+            </div>
 
-            <div>
+            <div className={styles.form__secret}>
               <span className={styles.form__label}>
                 비밀글 설정
               </span>
@@ -60,9 +96,9 @@ export default function Inquiry(): JSX.Element {
                 firstState={a}
                 toggleFn={toggle}
               />
-              <div>
-                {a ? 'ㅁㅁ' : 'ㄹㄹㄹ'}비밀글을 나의 문의 내역에서만 볼 수 있어요.
-              </div>
+            </div>
+            <div className={styles['form__secret-guide']}>
+              비밀글을 나의 문의 내역에서만 볼 수 있어요.
             </div>
 
             <div className={styles['form__submit-box']}>
