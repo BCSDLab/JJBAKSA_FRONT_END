7f24746e736203a4df5b033872635b5035bd921f
d0dam <smallkdb@gmail.com>
2023-12-13 11:55:51 +0900

    refactor: api 이해를 위한 주석 추가

--- src/api/scrap/index.ts
+++ src/api/scrap/index.ts
@@ -2,6 +2,7 @@ import scrapApi from './scrapApiClient';
 import type { GetMyScrapShopResponse } from './entity';
 
 export const postScrapShop = (shopId: string) =>
+  // post된 음식점이 들어가는 최상위 directoryId는 0입니다.
   scrapApi.post('/scraps', { directoryId: 0, placeId: shopId });
 
 export const getMyScrapShop = () => scrapApi.get<GetMyScrapShopResponse>('/scraps');
