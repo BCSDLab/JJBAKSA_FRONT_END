e745c1332dca00fc18cf082c68a6f20b8aef9222
정해성 <gotjd0230@naver.com>
2024-01-08 21:53:06 +0900

    refactor: 요청 부분 try catch 삭제

--- src/api/inquiry/inquire/index.ts
+++ src/api/inquiry/inquire/index.ts
@@ -2,29 +2,25 @@ import { SubmitInquiry } from '../entity';
 import inquiryApi from '../inquiryApiClient';
 
 const submitInquiry = async (inquiryData: SubmitInquiry) => {
-  try {
-    const formData = new FormData();
+  const formData = new FormData();
 
-    formData.append('title', inquiryData.title);
-    formData.append('content', inquiryData.content);
-    formData.append('isSecret', String(inquiryData.isSecret));
+  formData.append('title', inquiryData.title);
+  formData.append('content', inquiryData.content);
+  formData.append('isSecret', String(inquiryData.isSecret));
 
-    if (inquiryData.inquiryImages) {
-      inquiryData.inquiryImages.forEach((imageData) => {
-        formData.append('inquiryImages', imageData.imageUrl);
-      });
-    }
-
-    const response = await inquiryApi.post('/inquiry', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
+  if (inquiryData.inquiryImages) {
+    inquiryData.inquiryImages.forEach((imageData) => {
+      formData.append('inquiryImages', imageData.imageUrl);
     });
-
-    return response.data;
-  } catch (error) {
-    return null;
   }
+
+  const response = await inquiryApi.post('/inquiry', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+
+  return response.data;
 };
 
 export default submitInquiry;
--- src/api/inquiry/inquiry/index.ts
+++ src/api/inquiry/inquiry/index.ts
@@ -4,13 +4,9 @@ import inquiryApi from '../inquiryApiClient';
 const getInquiry = async ({
   queryType, dateCursor, idCursor, size,
 }: InquiryProps) => {
-  try {
-    const queryParams = `${queryType}?dateCursor=${dateCursor}&idCursor=${idCursor}&size=${size}`;
-    const { data } = await inquiryApi.get<GetInquiryResponse>(`/inquiry${queryParams}`);
-    return data;
-  } catch (error) {
-    return null;
-  }
+  const queryParams = `${queryType}?dateCursor=${dateCursor}&idCursor=${idCursor}&size=${size}`;
+  const { data } = await inquiryApi.get<GetInquiryResponse>(`/inquiry${queryParams}`);
+  return data;
 };
 
 export default getInquiry;
