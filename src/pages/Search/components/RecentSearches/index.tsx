639470ee5303f89a2d9e12b8d2614f11fb112305
chaeseungyun <thslr57@naver.com>
2024-01-10 16:20:59 +0900

    refactor: UI 수정

--- src/pages/Search/components/RecentSearches/index.tsx
+++ src/pages/Search/components/RecentSearches/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 
 import useMediaQuery from 'utils/hooks/useMediaQuery';
+import cn from 'utils/ts/classNames';
 
 import RecentItem from './components/RecentItem';
 import styles from './index.module.scss';
@@ -40,8 +41,15 @@ export default function RecentSearches() {
   return (
     <div className={styles.container}>
       <div className={styles.title}>
-        {isMobile ? <div className={styles.title__color}>최근 검색</div> : <div>최근 검색한 식당</div>}
-        <button type="button" onClick={clearStorage} className={styles.title__color}>전체 삭제</button>
+        {isMobile
+          ? <div className={styles.title__color}>최근 검색</div>
+          : <div className={styles.title__color}>최근 검색한 식당</div>}
+        <button
+          type="button"
+          onClick={clearStorage}
+          className={cn({ [styles.title__color]: true, [styles['title__color--mobile']]: !isMobile })}
+        >전체 삭제
+        </button>
       </div>
       <div className={styles.list}>
         {list && list.map((item, index) => (
