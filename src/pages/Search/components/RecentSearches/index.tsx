f1be55b2ef8a73cc89b63cc9d727fb88a87df540
chaeseungyun <thslr57@naver.com>
2024-01-10 15:47:37 +0900

    refactor: 모바일 버전 UI 수정

--- src/pages/Search/components/RecentSearches/index.tsx
+++ src/pages/Search/components/RecentSearches/index.tsx
@@ -40,8 +40,8 @@ export default function RecentSearches() {
   return (
     <div className={styles.container}>
       <div className={styles.title}>
-        {isMobile ? <div>최근 검색</div> : <div>최근 검색한 식당</div>}
-        <button type="button" onClick={clearStorage}>전체 삭제</button>
+        {isMobile ? <div className={styles.title__color}>최근 검색</div> : <div>최근 검색한 식당</div>}
+        <button type="button" onClick={clearStorage} className={styles.title__color}>전체 삭제</button>
       </div>
       <div className={styles.list}>
         {list && list.map((item, index) => (
