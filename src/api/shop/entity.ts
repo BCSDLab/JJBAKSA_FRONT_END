3407ecda419f5ff86cfbcd46b0162875017e82f2
Daeeui Kim <kimeodml@gmail.com>
2024-01-12 15:58:51 +0900

    fix: 불필요한 코드 제거

--- src/api/shop/entity.ts
+++ src/api/shop/entity.ts
@@ -51,7 +51,7 @@ export interface Shop {
   category: string; // 추후 카테고리 확인 필요
 }
 
-type Period = {
+export type Period = {
   closeTime: { hour: number; minute: number };
   openTime: { hour: number; minute: number };
 } | null;
--- src/pages/ShopDetail/index.tsx
+++ src/pages/ShopDetail/index.tsx
@@ -1,102 +1,105 @@
-import { useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 import { useQuery } from '@tanstack/react-query';
 
 import { fetchShop } from 'api/shop';
+import { Period } from 'api/shop/entity';
 import { ReactComponent as InfoIcon } from 'assets/svg/shop/info.svg';
 import AuthTopNavigation from 'components/Auth/AuthTopNavigation';
 import ImageCarousel from 'components/ImageCarousel';
-import StarRatingPreview from 'components/StarRating/StarRatingPreview';
+// import StarRatingPreview from 'components/StarRating/StarRatingPreview';
 import Map from 'pages/ShopDetail/components/Map/index';
 import FriendReviewList from 'pages/ShopDetail/components/ReviewList/FriendReviewList';
 import MyReviewList from 'pages/ShopDetail/components/ReviewList/MyReviewList';
 import ScrapButton from 'pages/ShopDetail/components/ScrapButton/index';
+import useScrapId from 'utils/hooks/useScrapId';
 
 import styles from './ShopDetail.module.scss';
 // import mock from './mock';
 
-const formatPeriod = (period: [number, number]) =>
-  period.map((time) => `${time.toString().slice(0, 2)}:${time.toString().slice(2)}`).join('~');
+const formatPeriod = (period: Period) => {
+  if (period) {
+    const formatTime = (time: number) => time.toString().padStart(2, '0');
+    const openTime = `${formatTime(period.openTime.hour)}:${formatTime(period.openTime.minute)}`;
+    const closeTime = `${formatTime(period.closeTime.hour)}:${formatTime(period.closeTime.minute)}`;
+    return `${openTime} - ${closeTime}`;
+  }
+  return '정보 없음';
+};
 
 function ShopDetail() {
-  const { placeId } = useParams();
+  const location = useLocation();
 
   const { data } = useQuery({
     queryKey: ['shopDetail'],
-    queryFn: () => fetchShop(placeId as string),
+    queryFn: () => fetchShop(location.state.placeId),
   });
 
-  if (data) {
+  const { scrapId } = useScrapId(String(location.state.placeId));
+
+  if (data && scrapId) {
     const {
-      // shopId,
-      // periods,
-      // openNow,
       // category,
-      // placeId,
-      scrap,
+      placeId,
+      coordinate,
       name,
       formattedAddress,
-      lat,
-      lng,
       formattedPhoneNumber,
-      totalRating,
+      // totalRating,
       todayPeriod,
       photos,
     } = data.data;
 
     return (
-      <>
-        <header className={styles.header}>
-          <AuthTopNavigation />
-        </header>
-
-        <div className={styles.container}>
-          <ImageCarousel pathname="shop" imageUrls={photos} />
-
-          <article className={styles['shop-detail']}>
-            <section className={styles['detail-main']}>
-              <div>
-                <div className={styles['detail-main__rating']}>
-                  <StarRatingPreview rate={totalRating} />
-                  {/* totalRating이 -1인 경우는 아예 별점이 없는 경우를 의미합니다. */}
-                  <span>{totalRating === -1 ? '0.0' : totalRating.toFixed(1)}</span>
-                </div>
-                <div className={styles['detail-main__name']}>
-                  <h1>{name}</h1>
-                  {placeId && <ScrapButton placeId={placeId} initialScrapId={scrap} />}
-                </div>
+      <div className={styles.container}>
+        <ImageCarousel pathname="shop" imageUrls={photos} />
+        <article className={styles['shop-detail']}>
+          <section className={styles['detail-main']}>
+            <div>
+              <div className={styles['detail-main__rating']}>
+                {/* <StarRatingPreview rate={totalRating} /> */}
+                {/* totalRating이 -1인 경우는 아예 별점이 없는 경우를 의미합니다. */}
+                {/* <span>{totalRating === -1 ? '0.0' : totalRating.toFixed(1)}</span> */}
               </div>
-
-              <div>
-                <div className={styles['detail-main__info-name']}>기본 정보</div>
-                <div className={styles['detail-main__info']}>
-                  <span>영업시간</span>
-                  <div className={styles['line-divisor']} />
-                  <span>{formatPeriod(todayPeriod)}</span>
-                </div>
-                <div className={styles['detail-main__info']}>
-                  <span>전화번호</span>
-                  <div className={styles['line-divisor']} />
-                  <span>{formattedPhoneNumber}</span>
-                </div>
-                <div className={styles['detail-main__info']}>
-                  <span>주소</span>
-                  <div className={styles['line-divisor']} />
-                  <span>{formattedAddress}</span>
-                </div>
+              <div className={styles['detail-main__name']}>
+                <h1>{name}</h1>
+                {placeId && <ScrapButton placeId={placeId} initialScrapId={scrapId.scrapId} />}
               </div>
-              <button className={styles['detail-main__report']} type="button" onClick={() => {}}>
-                <InfoIcon />
-                <div>틀린 정보 신고</div>
-              </button>
-            </section>
+            </div>
 
-            <FriendReviewList placeId={placeId as string} />
-            <Map formattedAddress={formattedAddress} latitude={lat} longitude={lng} />
-            <MyReviewList placeId={placeId as string} />
-          </article>
-        </div>
-      </>
+            <div>
+              <div className={styles['detail-main__info-name']}>기본 정보</div>
+              <div className={styles['detail-main__info']}>
+                <span>영업시간</span>
+                <div className={styles['line-divisor']} />
+                <span>{formatPeriod(todayPeriod)}</span>
+              </div>
+              <div className={styles['detail-main__info']}>
+                <span>전화번호</span>
+                <div className={styles['line-divisor']} />
+                <span>{formattedPhoneNumber ?? '정보 없음'}</span>
+              </div>
+              <div className={styles['detail-main__info']}>
+                <span>주소</span>
+                <div className={styles['line-divisor']} />
+                <span>{formattedAddress}</span>
+              </div>
+            </div>
+            <button className={styles['detail-main__report']} type="button" onClick={() => {}}>
+              <InfoIcon />
+              <div>틀린 정보 신고</div>
+            </button>
+          </section>
+
+          <FriendReviewList placeId={placeId as string} />
+          <Map
+            formattedAddress={formattedAddress}
+            latitude={coordinate.lat}
+            longitude={coordinate.lng}
+          />
+          <MyReviewList placeId={placeId as string} />
+        </article>
+      </div>
     );
   }
 
