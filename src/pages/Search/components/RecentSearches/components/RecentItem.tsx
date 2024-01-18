527febe1ce548ff297a3e7758ebdc975b62c4134
chaeseungyun <thslr57@naver.com>
2024-01-18 23:37:07 +0900

    refactor: 최근 검색한 식당 클릭 시 식당 상세정보로 이동

--- src/pages/Search/components/RecentSearches/components/RecentItem.tsx
+++ src/pages/Search/components/RecentSearches/components/RecentItem.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import defaultImage from 'assets/images/search/default-image.png';
 import { ReactComponent as Clock } from 'assets/svg/common/clock.svg';
 import { ReactComponent as Close } from 'assets/svg/common/close.svg';
@@ -19,18 +21,20 @@ export default function RecentItem({
 }: Props) {
   if (index < 5) {
     return (
-      <div className={styles.container}>
-        {!isMobile && <img alt="imageAlt" src={photoToken ?? defaultImage} className={styles.image} />}
-        <div className={styles.description}>
-          {isMobile ? <Clock /> : <div className={styles.description__category}>{category}</div>}
-          <div className={styles.description__name}>{name}</div>
-        </div>
-        {isMobile ? <button className={styles.delete} type="button" onClick={() => deleteItem(placeId)}>x</button> : (
-          <div className={styles.cover}>
-            <Close onClick={() => deleteItem(placeId)} className={styles.cover__delete} />
+      <Link to={`/shop/${placeId}`} state={{ placeId }}>
+        <div className={styles.container}>
+          {!isMobile && <img alt="imageAlt" src={photoToken ?? defaultImage} className={styles.image} />}
+          <div className={styles.description}>
+            {isMobile ? <Clock /> : <div className={styles.description__category}>{category}</div>}
+            <div className={styles.description__name}>{name}</div>
           </div>
-        )}
-      </div>
+          {isMobile ? <button className={styles.delete} type="button" onClick={() => deleteItem(placeId)}>x</button> : (
+            <div className={styles.cover}>
+              <Close onClick={() => deleteItem(placeId)} className={styles.cover__delete} />
+            </div>
+          )}
+        </div>
+      </Link>
     );
   } return null;
 }
