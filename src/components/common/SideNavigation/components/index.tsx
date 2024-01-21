932311738fcaa564ff01907bf83efa79e3791be6
Daeeui Kim <kimeodml@gmail.com>
2024-01-21 13:27:48 +0900

    feat: 별점 추가

--- src/components/common/SideNavigation/components/index.tsx
+++ src/components/common/SideNavigation/components/index.tsx
@@ -1,6 +1,8 @@
+import { ReactComponent as StarIcon } from 'assets/svg/post/star.svg';
 import { ReactComponent as BookMarkIcon } from 'assets/svg/shop/book-mark.svg';
 import usePin from 'components/common/SideNavigation/hooks/usePin';
 import ImageCarousel from 'components/ImageCarousel';
+import useRate from 'utils/hooks/useRate';
 import useScrap from 'utils/hooks/useScrap';
 import useScrapId from 'utils/hooks/useScrapId';
 
@@ -13,24 +15,30 @@ interface PinProps {
 export default function Pin({ placeId }:PinProps): JSX.Element {
   const { data } = usePin(String(placeId));
   const { scrapId } = useScrapId(String(placeId));
+  const { rate } = useRate(placeId);
 
   const { toggleScrap, isPending } = useScrap(String(placeId), Number(scrapId));
   return (
     <>
       <ImageCarousel pathname="pin" imageUrls={data?.photos} />
-      <div className={styles.container}>
-        <div className={styles.container__name}>{data?.name}</div>
-        <div className={styles.container__category}>{data?.category}</div>
-        <button
-          type="button"
-          onClick={toggleScrap}
-          disabled={isPending}
-          className={styles['container__scrap-button']}
-        >
-          {scrapId ? <BookMarkIcon fill="#FF7F23" /> : <BookMarkIcon stroke="#FF7F23" />}
-          <div>북마크</div>
-        </button>
-      </div>
+      <ul className={styles.container}>
+        <li className={styles.info}>
+          <span className={styles.info__name}>{data?.name}</span>
+          <span className={styles.info__category}>{data?.category}</span>
+          <button
+            type="button"
+            onClick={toggleScrap}
+            disabled={isPending}
+            className={styles['info__scrap-button']}
+          >
+            {scrapId ? <BookMarkIcon fill="#FF7F23" /> : <BookMarkIcon stroke="#FF7F23" />}
+            <div>북마크</div>
+          </button>
+        </li>
+        <li className={styles.info}>
+          <div className={styles.info__rate}><StarIcon fill="#FF7F23" width="18" height="18" /> {rate}.0 마지막 리뷰</div>
+        </li>
+      </ul>
     </>
   );
 }
