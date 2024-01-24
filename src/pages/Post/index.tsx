4a6a0dc29ce80a5e89c9d6a0f41e1ffe78e71d74
Daeeui Kim <kimeodml@gmail.com>
2024-01-24 19:51:01 +0900

    fix:  post쪽  음식점 이름 수정

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,8 +1,10 @@
 import { useNavigate, useParams } from 'react-router-dom';
 
-import { useQueryClient } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 
 import { postReview } from 'api/review';
+import { fetchShop } from 'api/shop';
+import LoadingSpinner from 'components/common/LoadingSpinner';
 import TextEditor from 'components/editor/TextEditor';
 import { useReview } from 'store/review';
 import makeToast from 'utils/ts/makeToast';
@@ -10,13 +12,17 @@ import makeToast from 'utils/ts/makeToast';
 import styles from './Post.module.scss';
 
 export default function Post() {
-  const { name: shopName } = useParams();
+  const { name: placeId } = useParams();
+  const { data } = useQuery({
+    queryKey: ['shopDetail', placeId],
+    queryFn: () => fetchShop(placeId as string),
+  });
   const queryClient = useQueryClient();
   const navigate = useNavigate();
   const review = useReview();
   const submitReview = () => {
     postReview({
-      placeId: shopName as string,
+      placeId: placeId as string,
       ...review,
     }).then(() => {
       queryClient.invalidateQueries({ queryKey: ['reviewedShops'] });
@@ -27,9 +33,16 @@ export default function Post() {
     });
   };
 
+  if (data) {
+    return (
+      <div className={styles.post}>
+        <TextEditor shop={data?.data.name!} onSubmit={submitReview} />
+      </div>
+    );
+  }
   return (
-    <div className={styles.post}>
-      <TextEditor shop={shopName!} onSubmit={submitReview} />
+    <div className={styles.loading}>
+      <LoadingSpinner size={200} />
     </div>
   );
 }
