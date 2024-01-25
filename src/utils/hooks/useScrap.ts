c58d9cc196f2ca77a251ac040eafeed1f9b85b2d
LUNA-KK <03somin0324@gmail.com>
2024-01-25 22:15:32 +0900

    fix: 캐싱값 무효화

--- src/utils/hooks/useScrap.ts
+++ src/utils/hooks/useScrap.ts
@@ -1,15 +1,19 @@
 import { useState } from 'react';
 
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { deleteScrapShop, postScrapShop } from 'api/scrap';
 
 const useScrap = (placeId: string, initialScrapId: number | null) => {
   const [scrapId, setScrapId] = useState<number | null>(initialScrapId);
-
+  const queryClient = useQueryClient();
   const { mutate: postScrap, isPending: postPending } = useMutation({
+    mutationKey: ['bookMark'],
     mutationFn: postScrapShop,
-    onSuccess: (res) => setScrapId(res.data.id),
+    onSuccess: (res) => {
+      setScrapId(res.data.id);
+      queryClient.invalidateQueries({ queryKey: ['scraps'] });
+    },
   });
 
   const { mutate: deleteScrap, isPending: deletePending } = useMutation({
