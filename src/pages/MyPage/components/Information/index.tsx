c18e4f3beca78c9679b2da2895ca87010f611f53
chaeseungyun <thslr57@naver.com>
2024-01-26 21:37:18 +0900

    refactor: 마우스 hover 시 연필 아이콘 노출

--- src/pages/MyPage/components/Information/index.tsx
+++ src/pages/MyPage/components/Information/index.tsx
@@ -2,6 +2,7 @@ import { Link, useNavigate } from 'react-router-dom';
 
 import { User } from 'api/user/entity';
 import defaultImage from 'assets/images/follow/default-image.png';
+import { ReactComponent as Pencil } from 'assets/svg/common/pencil.svg';
 import option from 'assets/svg/mypage/option.svg';
 
 import styles from './Information.module.scss';
@@ -25,6 +26,9 @@ export default function Information({ openModal, followerNumber, profile }: Info
       <div className={styles.user}>
         <button type="button" className={styles.profile} onClick={() => openModal(profile.profileImage?.url)}>
           <img src={profile.profileImage ? profile.profileImage.url : defaultImage} alt="profileImage" className={styles.user__image} />
+          <div className={styles.profile__pencil}>
+            <Pencil />
+          </div>
         </button>
         <div>
           <span className={styles.user__nickname}>
