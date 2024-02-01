45e62db51840a9b589258f2baeea17dacd1d1059
정해성 <96577250+junghaesung79@users.noreply.github.com>
2024-02-02 01:11:35 +0900

    refactor: 내보낼 수 없는 정보 UI에서 주석처리
    - 스타일 개편
    - 필요한 정보 넣기

--- src/pages/SearchDetails/components/SearchItem/SearchItem.tsx
+++ src/pages/SearchDetails/components/SearchItem/SearchItem.tsx
@@ -2,8 +2,9 @@ import { useLocation, useNavigate } from 'react-router-dom';
 
 import { ShopQueryResponse } from 'api/shop/entity';
 import defaultImage from 'assets/svg/common/favicon.svg';
-import { ReactComponent as PhoneIcon } from 'assets/svg/search/phone.svg';
+// import { ReactComponent as PhoneIcon } from 'assets/svg/search/phone.svg';
 import { ReactComponent as Star } from 'assets/svg/search/star.svg';
+import useShop from 'pages/Post/hooks/useShop';
 import { Card } from 'pages/Search/static/entity';
 import useMediaQuery from 'utils/hooks/useMediaQuery';
 import cn from 'utils/ts/classNames';
@@ -17,27 +18,36 @@ interface Props {
 }
 
 export default function SearchItem({ shop, addCard }: Props) {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { isMobile } = useMediaQuery();
+
   const {
     name, formattedAddress, photoToken,
     placeId, dist, openNow, category,
   } = shop;
-  const { rate } = useShopRate(placeId);
-  const formattedPhoneNumber = null;
 
-  const navigate = useNavigate();
-  const location = useLocation();
-  const { isMobile } = useMediaQuery();
+  const { todayPeriod } = useShop(placeId);
+  const { rate } = useShopRate(placeId);
+  const newPath = location.pathname.includes('/shop') ? `/shop/${placeId}` : `/post/${placeId}`;
   const distInKm = (dist / 1000).toFixed(1);
-  const safePhoneNumber = formattedPhoneNumber && formattedPhoneNumber !== '' ? formattedPhoneNumber : 'No Phone Number';
+
+  // api 없음 생략
+  // const { reviewCount } = useReviewCount(placeId);
+  // const safePhoneNumber = formattedPhoneNumber !== '' ? formattedPhoneNumber : '전화번호 정보 없음';
+
+  const openTime = todayPeriod
+    ? `${String(todayPeriod.openTime.hour).padStart(2, '0')}:${String(todayPeriod.openTime.minute).padStart(2, '0')}에 영업 시작`
+    : '시작 정보 없음';
+  const closeTime = todayPeriod
+    ? `${String(todayPeriod.closeTime.hour).padStart(2, '0')}:${String(todayPeriod.closeTime.minute).padStart(2, '0')}에 영업 종료`
+    : '종료 정보 없음';
 
   const handleClick = () => {
     const card: Card = {
       category, name, photoToken, placeId,
     };
-
     addCard(card);
-
-    const newPath = location.pathname.includes('/shop') ? `/shop/${placeId}` : `/post/${placeId}`;
     navigate(newPath);
   };
 
@@ -68,6 +78,7 @@ export default function SearchItem({ shop, addCard }: Props) {
                 |
               </>
             )}
+
             <div
               className={cn({
                 [styles.state__operating]: true,
@@ -76,29 +87,36 @@ export default function SearchItem({ shop, addCard }: Props) {
             >
               {openNow ? '영업중' : '영업 종료'}
             </div>
-            {!isMobile && <>|<div className={styles.state__time}>운영 시간</div></> }
-          </div>
 
-          {!isMobile && (
+            {!isMobile && (
+              <>
+                |
+                <div className={styles.state__time}>
+                  {openNow ? openTime : closeTime}
+                </div>
+              </>
+            )}
+          </div>
+          {/* {!isMobile && (
             <div className={`${styles.info__call} ${styles.call}`}>
               <PhoneIcon className={styles.call__icon} />
               <div className={styles.call__number}>{safePhoneNumber}</div>
             </div>
-          )}
+          )} */}
         </div>
-        <div className={`${styles.box__pictures} ${styles.pictures}`}>
-          <picture className={styles.pictures__picture}>
-            <img className={styles.pictures__image} alt="가게 이미지" src={photoToken ?? defaultImage} />
-          </picture>
-          {/* <picture className={styles.pictures__picture}>
-            <source srcSet={defaultImage} />
-            <img className={styles.pictures__image} alt="가게 이미지" />
-          </picture>
-          <picture className={styles.pictures__picture}>
-            <source srcSet={defaultImage} />
-            <img className={styles.pictures__image} alt="가게 이미지" />
-          </picture> */}
+
+        <div className={`${styles.box__images} ${styles.images}`}>
+          <img
+            className={styles.images__image}
+            alt="가게 이미지"
+            src={photoToken ?? defaultImage}
+            onError={(e) => {
+              const target = e.target as HTMLImageElement;
+              target.src = defaultImage;
+            }}
+          />
         </div>
+
         {/* {isMobile && (
           <div className={`${styles.box__review} ${styles.review}`}>
             {review ? `${$reviewApi.length}개의 리뷰` : '리뷰 없음'}
