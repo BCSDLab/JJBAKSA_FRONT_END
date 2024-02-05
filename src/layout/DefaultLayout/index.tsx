31cc8d41694e61f5e9f76babee3e2858ba14d90f
Daeeui Kim <kimeodml@gmail.com>
2024-02-05 15:51:21 +0900

    refactor: Home 컴포넌트 경로 추가

--- src/layout/DefaultLayout/index.tsx
+++ src/layout/DefaultLayout/index.tsx
@@ -1,25 +1,18 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import BottomNavigation from 'components/common/BottomNavigation';
 import SideNavigation from 'components/common/SideNavigation';
-import Home from 'pages/Home';
 import useMediaQuery from 'utils/hooks/useMediaQuery';
-import cn from 'utils/ts/classNames';
 
 import styles from './DefaultLayout.module.scss';
 
 export default function DefaultLayout(): JSX.Element {
   const { isMobile } = useMediaQuery();
-  const location = useLocation();
-
   return (
     <>
       {!isMobile && <span className={styles['sidebar-space']} />}
-      <Outlet />
-      <div className={cn({ [styles.home]: location.pathname !== '/' })}>
-        <Home />
-      </div>
       {!isMobile && <SideNavigation />}
+      <Outlet />
       {isMobile && <BottomNavigation />}
     </>
   );
