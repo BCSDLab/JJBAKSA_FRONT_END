7a51f52b263b62940423c0672d40ee312d70eced
Daeeui Kim <kimeodml@gmail.com>
2024-03-05 15:17:08 +0900

    style: margin-top: 80px 추가

--- src/layout/TopBottomDefaultLayout/index.tsx
+++ src/layout/TopBottomDefaultLayout/index.tsx
@@ -4,12 +4,16 @@ import BottomNavigation from 'components/common/BottomNavigation';
 import TopNavigation from 'components/common/TopNavigation';
 import useMediaQuery from 'utils/hooks/useMediaQuery';
 
+import styles from './TopBottomDefaultLayout.module.scss';
+
 export default function TopBottomDefaultLayout(): JSX.Element {
   const { isMobile } = useMediaQuery();
   return (
     <>
       {!isMobile && <TopNavigation />}
-      <Outlet />
+      <main className={styles.main}>
+        <Outlet />
+      </main>
       {isMobile && <BottomNavigation />}
     </>
   );
--- src/layout/TopDefaultLayout/index.tsx
+++ src/layout/TopDefaultLayout/index.tsx
@@ -3,12 +3,16 @@ import { Outlet } from 'react-router-dom';
 import TopNavigation from 'components/common/TopNavigation';
 import useMediaQuery from 'utils/hooks/useMediaQuery';
 
+import styles from './TopDefaultLayout.module.scss';
+
 export default function TopDefaultLayout(): JSX.Element {
   const { isMobile } = useMediaQuery();
   return (
     <>
       {!isMobile && <TopNavigation />}
-      <Outlet />
+      <main className={styles.main}>
+        <Outlet />
+      </main>
     </>
   );
 }
