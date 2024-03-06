bfc111c9fd19f6049968ef8aeee0a5d0a52079fc
Daeeui Kim <kimeodml@gmail.com>
2024-03-06 18:07:40 +0900

    fix: 마커 클러스터링 수정

--- src/pages/Home/Map/hooks/useCluster.ts
+++ src/pages/Home/Map/hooks/useCluster.ts
@@ -1,6 +1,6 @@
 /* eslint-disable consistent-return */
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ClusterHtml } from 'pages/Home/Map/components/MarkerHtml/index';
 import MarkerClustering from 'utils/js/MarkerClustering';
 
@@ -16,37 +16,37 @@ const HTMLMARKER = {
 };
 
 function useCluster({ markerArray, map }: ClusterProps) {
-  const [cluster, setCluster] = useState<MarkerClustering | null>(null);
-  const clusterElementRef = useRef<HTMLDivElement | null>(null);
+  const [cluster, setCluster] = useState<any | null>(null);
 
   useEffect(() => {
-    const markerClustering = new MarkerClustering({
-      minClusterSize: 2,
-      maxZoom: 20,
-      map,
-      markers: markerArray,
-      disableClickZoom: false,
-      gridSize: 120,
-      icons: [HTMLMARKER],
-      indexGenerator: [120],
-      stylingFunction: (clusterMarker: any, count: number) => {
-        if (clusterMarker) {
-          const firstChild = clusterMarker.getElement().querySelector('div:first-child');
-          if (firstChild) {
-            firstChild.innerHTML = count;
-            clusterElementRef.current = firstChild;
+    if (map && markerArray.length > 0) {
+      const markerClustering = new MarkerClustering({
+        minClusterSize: 2,
+        maxZoom: 20,
+        map,
+        markers: markerArray,
+        disableClickZoom: false,
+        gridSize: 120,
+        icons: [HTMLMARKER],
+        indexGenerator: [120],
+        stylingFunction: (clusterMarker: any, count: number) => {
+          if (clusterMarker) {
+            const firstChild = clusterMarker.getElement().querySelector('div:first-child');
+            if (firstChild) {
+              firstChild.innerHTML = count;
+            }
           }
-        }
-      },
-    });
+        },
+      });
 
-    setCluster(markerClustering);
+      setCluster(markerClustering);
+    }
+  }, [map, markerArray]);
 
-    return () => {
-      if (clusterElementRef.current) {
-        clusterElementRef.current.remove();
-      }
-    };
+  useEffect(() => {
+    if (cluster) {
+      cluster.setMap(null);
+    }
   }, [map, markerArray]);
 
   return { cluster };
